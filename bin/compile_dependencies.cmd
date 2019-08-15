fcf29ba2aaa87b1ce96b072b8c96f4f0bc9619b3
Joeri Bekker <joeri@maykinmedia.nl>
2019-08-15 11:35:23 +0200

    :wrench: Fixes #1 -- Added Travis configuration and removed Jenkins references.

--- bin/compile_dependencies.cmd
+++ bin/compile_dependencies.cmd
@@ -8,19 +8,3 @@ REM Base deps
 pip-compile^
     --no-index^
     requirements/base.in
-
-REM Dev deps
-pip-compile^
-    --no-index^
-    --output-file requirements/dev.txt^
-    requirements/base.txt^
-    requirements/testing.in^
-    requirements/dev.in
-
-REM Jenkins/tests deps
-pip-compile^
-    --no-index^
-    --output-file requirements/jenkins.txt^
-    requirements/base.txt^
-    requirements/dev.txt^
-    requirements/jenkins.in
