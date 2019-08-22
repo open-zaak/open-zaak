b7916c352e4208e05fd621ac2317ccd1437b57d9
Sergei Maertens <sergei@maykinmedia.nl>
2019-08-22 16:35:01 +0200

    :white_check_mark: Refs. #5 -- added integrity tests for OIO

---
+++ src/openzaak/components/documenten/models/tests/test_oio.py
@@ -0,0 +1,52 @@
+from django.db import IntegrityError
+from django.test import TestCase, tag
+
+from openzaak.components.besluiten.models.tests.factories import (
+    BesluitFactory, BesluitInformatieObjectFactory
+)
+from openzaak.components.documenten.models.tests.factories import (
+    EnkelvoudigInformatieObjectCanonicalFactory
+)
+from openzaak.components.zaken.models.tests.factories import (
+    ZaakFactory, ZaakInformatieObjectFactory
+)
+
+from ..models import ObjectInformatieObject
+
+
+@tag("oio")
+class OIOTests(TestCase):
+
+    def test_not_both_zaak_besluit(self):
+        canonical = EnkelvoudigInformatieObjectCanonicalFactory.create()
+        zaak = ZaakFactory.create()
+        besluit = BesluitFactory.create()
+
+        with self.assertRaises(IntegrityError):
+            ObjectInformatieObject.objects.create(
+                informatieobject=canonical,
+                zaak=zaak,
+                besluit=besluit,
+            )
+
+    def test_either_zaak_or_besluit_required(self):
+        canonical = EnkelvoudigInformatieObjectCanonicalFactory.create()
+
+        with self.assertRaises(IntegrityError):
+            ObjectInformatieObject.objects.create(informatieobject=canonical, zaak=None, besluit=None)
+
+    def test_zio_creates_oio(self):
+        zio = ZaakInformatieObjectFactory.create()
+
+        oio = ObjectInformatieObject.objects.get()
+
+        self.assertEqual(oio.informatieobject, zio.informatieobject)
+        self.assertEqual(oio.object, zio.zaak)
+
+    def test_bio_creates_oio(self):
+        bio = BesluitInformatieObjectFactory.create()
+
+        oio = ObjectInformatieObject.objects.get()
+
+        self.assertEqual(oio.informatieobject, bio.informatieobject)
+        self.assertEqual(oio.object, bio.besluit)
