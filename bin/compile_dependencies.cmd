33c38384fd91a94dc404179e2b9c098a6029290e
Sergei Maertens <sergei@maykinmedia.nl>
2019-08-28 18:05:05 +0200

    :pushpin: Fixes #55 -- Pin all (dev/test/production) dependencies
    Interesting things happen if the minor versions of dev-tooling aren't
    the same in all developers environments, which is why _everything_
    needs to be pinned.

--- bin/compile_dependencies.cmd
+++ bin/compile_dependencies.cmd
@@ -8,3 +8,17 @@ REM Base deps
 pip-compile^
     --no-index^
     requirements/base.in
+
+REM Dependencies for testing
+pip-compile^
+    --no-index^
+    --output-file requirements/test.txt^
+    requirements/base.txt^
+    requirements/testing.in
+
+REM Dev depedencies - exact same set as CI + some extra tooling
+pip-compile^
+    --no-index^
+    --output-file requirements/dev.txt^
+    requirements/test.txt^
+    requirements/dev.in
--- bin/compile_dependencies.sh
+++ bin/compile_dependencies.sh
@@ -6,7 +6,21 @@ toplevel=$(git rev-parse --show-toplevel)
 
 cd $toplevel
 
-# Base deps
+# Base (& prod) deps
 pip-compile \
     --no-index \
     requirements/base.in
+
+# Dependencies for testing
+pip-compile \
+    --no-index \
+    --output-file requirements/test.txt \
+    requirements/base.txt \
+    requirements/testing.in
+
+# Dev depedencies - exact same set as CI + some extra tooling
+pip-compile \
+    --no-index \
+    --output-file requirements/dev.txt \
+    requirements/test.txt \
+    requirements/dev.in
