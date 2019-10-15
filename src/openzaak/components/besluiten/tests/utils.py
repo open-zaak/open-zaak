c0b3fd0189fcbf6206848a953f91e549f96b37fe
Sergei Maertens <sergei@maykinmedia.nl>
2019-10-15 18:20:16 +0200

    :white_check_mark: Add test for authorized loader calls

--- src/openzaak/components/besluiten/tests/utils.py
+++ src/openzaak/components/besluiten/tests/utils.py
@@ -7,3 +7,23 @@ def get_operation_url(operation, **kwargs):
     return _get_operation_url(
         operation, spec_path=settings.SPEC_URL["besluiten"], **kwargs
     )
+
+
+def get_besluittype_response(catalogus: str, besluittype: str) -> dict:
+    return {
+        "url": besluittype,
+        "catalogus": catalogus,
+        "zaaktypes": [],
+        "omschrijving": "Extern Besluittype",
+        "omschrijvingGeneriek": "",
+        "besluitcategorie": "",
+        "reactietermijn": "P14D",
+        "publicatieIndicatie": True,
+        "publicatietekst": "",
+        "publicatietermijn": None,
+        "toelichting": "",
+        "informatieobjecttypes": [],
+        "beginGeldigheid": "2018-01-01",
+        "eindeGeldigheid": None,
+        "concept": False,
+    }
