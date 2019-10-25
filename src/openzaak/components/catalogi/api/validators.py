015bf57b0d8a303a90be80ea9b256c9dd98b45d8
Steven Bal <steven@maykinmedia.nl>
2019-10-25 09:24:25 +0200

    Fix ZaakInformatieobjecttypeCatalogus validator for PATCH operations

--- src/openzaak/components/catalogi/api/validators.py
+++ src/openzaak/components/catalogi/api/validators.py
@@ -271,9 +271,18 @@ class ZaakInformatieObjectTypeCatalogusValidator:
     code = "relations-incorrect-catalogus"
     message = _("The zaaktype has catalogus different from informatieobjecttype")
 
+    def set_context(self, serializer):
+        """
+        This hook is called by the serializer instance,
+        prior to the validation call being made.
+        """
+        self.instance = getattr(serializer, "instance", None)
+
     def __call__(self, attrs: dict):
-        zaaktype = attrs.get("zaaktype")
-        informatieobjecttype = attrs.get("informatieobjecttype")
+        zaaktype = attrs.get("zaaktype") or self.instance.zaaktype
+        informatieobjecttype = (
+            attrs.get("informatieobjecttype") or self.instance.informatieobjecttype
+        )
 
         if zaaktype.catalogus != informatieobjecttype.catalogus:
             raise ValidationError(self.message, code=self.code)
