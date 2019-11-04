f9d7fec9d10753a2258f750745f72eb907a17bca
annashamray <anna.shamray@maykinmedia.nl>
2019-11-04 14:39:48 +0100

    remove Audit from Oio resource since it is not used

--- src/openzaak/components/documenten/tests/test_audittrails.py
+++ src/openzaak/components/documenten/tests/test_audittrails.py
@@ -75,43 +75,6 @@ class AuditTrailTests(JWTAuthMixin, APITestCase):
             informatieobject_create_audittrail.nieuw, informatieobject_data
         )
 
-    @tag("oio")
-    def test_create_objectinformatieobject_audittrail(self):
-        informatieobject = EnkelvoudigInformatieObjectFactory.create()
-        zio = ZaakInformatieObjectFactory.create(
-            informatieobject=informatieobject.canonical
-        )
-
-        content = {
-            "informatieobject": reverse(
-                "enkelvoudiginformatieobject-detail",
-                kwargs={"uuid": informatieobject.uuid},
-            ),
-            "object": reverse(zio.zaak),
-            "objectType": ObjectTypes.zaak,
-        }
-
-        # Send to the API
-        objectinformatieobject_response = self.client.post(
-            self.objectinformatieobject_list_url, content
-        ).data
-
-        informatieobject_url = objectinformatieobject_response["informatieobject"]
-        audittrails = AuditTrail.objects.filter(hoofd_object=informatieobject_url)
-        self.assertEqual(audittrails.count(), 1)
-
-        # Verify that the audittrail for the ObjectInformatieObject creation
-        # contains the correct information
-        objectinformatieobject_create_audittrail = audittrails.get()
-        self.assertEqual(objectinformatieobject_create_audittrail.bron, "DRC")
-        self.assertEqual(objectinformatieobject_create_audittrail.actie, "create")
-        self.assertEqual(objectinformatieobject_create_audittrail.resultaat, 201)
-        self.assertEqual(objectinformatieobject_create_audittrail.oud, None)
-        self.assertEqual(
-            objectinformatieobject_create_audittrail.nieuw,
-            objectinformatieobject_response,
-        )
-
     def test_create_and_delete_gebruiksrechten_audittrail(self):
         informatieobject = EnkelvoudigInformatieObjectFactory.create()
 
