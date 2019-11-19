ad4d94a95b2501e59d7a1e445870722f26ad53d3
Steven Bal <steven@maykinmedia.nl>
2019-11-19 14:24:28 +0100

    Formatting with black

--- src/openzaak/components/catalogi/migrations/0007_auto_20191119_1136.py
+++ src/openzaak/components/catalogi/migrations/0007_auto_20191119_1136.py
@@ -7,28 +7,37 @@ from django.db import models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('catalogi', '0006_auto_20191024_1000'),
+        ("catalogi", "0006_auto_20191024_1000"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='besluittype',
-            name='zaaktypes',
-            field=models.ManyToManyField(help_text='ZAAKTYPE met ZAAKen die relevant kunnen zijn voor dit BESLUITTYPE', related_name='besluittypen', to='catalogi.ZaakType', verbose_name='zaaktypen'),
+            model_name="besluittype",
+            name="zaaktypes",
+            field=models.ManyToManyField(
+                help_text="ZAAKTYPE met ZAAKen die relevant kunnen zijn voor dit BESLUITTYPE",
+                related_name="besluittypen",
+                to="catalogi.ZaakType",
+                verbose_name="zaaktypen",
+            ),
         ),
         migrations.RenameField(
-            model_name='besluittype',
-            old_name='zaaktypes',
-            new_name='zaaktypen',
+            model_name="besluittype", old_name="zaaktypes", new_name="zaaktypen",
         ),
         migrations.AlterField(
-            model_name='informatieobjecttype',
-            name='zaaktypes',
-            field=models.ManyToManyField(help_text='ZAAKTYPE met ZAAKen die relevant kunnen zijn voor dit INFORMATIEOBJECTTYPE', related_name='informatieobjecttypen', through='catalogi.ZaakInformatieobjectType', to='catalogi.ZaakType', verbose_name='zaaktypen'),
+            model_name="informatieobjecttype",
+            name="zaaktypes",
+            field=models.ManyToManyField(
+                help_text="ZAAKTYPE met ZAAKen die relevant kunnen zijn voor dit INFORMATIEOBJECTTYPE",
+                related_name="informatieobjecttypen",
+                through="catalogi.ZaakInformatieobjectType",
+                to="catalogi.ZaakType",
+                verbose_name="zaaktypen",
+            ),
         ),
         migrations.RenameField(
-            model_name='informatieobjecttype',
-            old_name='zaaktypes',
-            new_name='zaaktypen',
+            model_name="informatieobjecttype",
+            old_name="zaaktypes",
+            new_name="zaaktypen",
         ),
     ]
