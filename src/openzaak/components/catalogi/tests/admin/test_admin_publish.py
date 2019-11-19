c35b35c9080a8b7d931022a19772d0e01f25dde5
Steven Bal <steven@maykinmedia.nl>
2019-11-19 16:03:46 +0100

    Added tests for publishing catalogi in admin

---
+++ src/openzaak/components/catalogi/tests/admin/test_admin_publish.py
@@ -0,0 +1,174 @@
+from django.urls import reverse
+
+import requests_mock
+from django_webtest import WebTest
+
+from openzaak.accounts.tests.factories import SuperUserFactory
+from openzaak.selectielijst.tests import mock_oas_get, mock_resource_list
+from openzaak.utils.tests import ClearCachesMixin
+
+from ..factories import (
+    BesluitTypeFactory,
+    InformatieObjectTypeFactory,
+    ZaakInformatieobjectTypeFactory,
+    ZaakTypeFactory,
+)
+
+
+@requests_mock.Mocker()
+class ZaaktypeAdminTests(ClearCachesMixin, WebTest):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = SuperUserFactory.create()
+
+    def setUp(self):
+        super().setUp()
+
+        self.app.set_user(self.user)
+
+    def test_publish_zaaktype(self, m):
+        mock_oas_get(m)
+        mock_resource_list(m, "procestypen")
+
+        zaaktype = ZaakTypeFactory.create(
+            concept=True,
+            zaaktype_omschrijving="test",
+            vertrouwelijkheidaanduiding="openbaar",
+            trefwoorden=["test"],
+            verantwoordingsrelatie=["bla"],
+        )
+        url = reverse("admin:catalogi_zaaktype_change", args=(zaaktype.pk,))
+
+        response = self.app.get(url)
+
+        # Verify that the publish button is visible
+        publish_button = response.html.find("input", {"name": "_publish"})
+        self.assertIsNotNone(publish_button)
+
+        form = response.forms["zaaktype_form"]
+
+        response = form.submit("_publish").follow()
+
+        zaaktype.refresh_from_db()
+        self.assertFalse(zaaktype.concept)
+
+        # Verify that the publish button is not on the page anymore
+        publish_button = response.html.find("input", {"name": "_publish"})
+        self.assertIsNone(publish_button)
+
+    def test_publish_besluittype(self, m):
+        besluittype = BesluitTypeFactory.create(concept=True)
+        url = reverse("admin:catalogi_besluittype_change", args=(besluittype.pk,))
+
+        response = self.app.get(url)
+
+        # Verify that the publish button is visible
+        publish_button = response.html.find("input", {"name": "_publish"})
+        self.assertIsNotNone(publish_button)
+
+        form = response.forms["besluittype_form"]
+
+        response = form.submit("_publish").follow()
+
+        besluittype.refresh_from_db()
+        self.assertFalse(besluittype.concept)
+
+        # Verify that the publish button is not on the page anymore
+        publish_button = response.html.find("input", {"name": "_publish"})
+        self.assertIsNone(publish_button)
+
+    def test_publish_informatieobjecttype(self, m):
+        iot = InformatieObjectTypeFactory.create(
+            concept=True, vertrouwelijkheidaanduiding="openbaar"
+        )
+        iot.zaakinformatieobjecttype_set.all().delete()
+        url = reverse("admin:catalogi_informatieobjecttype_change", args=(iot.pk,))
+
+        response = self.app.get(url)
+
+        # Verify that the publish button is visible
+        publish_button = response.html.find("input", {"name": "_publish"})
+        self.assertIsNotNone(publish_button)
+
+        form = response.forms["informatieobjecttype_form"]
+
+        response = form.submit("_publish").follow()
+
+        iot.refresh_from_db()
+        self.assertFalse(iot.concept)
+
+        # Verify that the publish button is not on the page anymore
+        publish_button = response.html.find("input", {"name": "_publish"})
+        self.assertIsNone(publish_button)
+
+    def test_publish_zaaktype_related_to_concept_besluittype_fails(self, m):
+        mock_oas_get(m)
+        mock_resource_list(m, "procestypen")
+
+        zaaktype = ZaakTypeFactory.create(
+            concept=True,
+            zaaktype_omschrijving="test",
+            vertrouwelijkheidaanduiding="openbaar",
+            trefwoorden=["test"],
+            verantwoordingsrelatie=["bla"],
+        )
+        BesluitTypeFactory.create(concept=True, zaaktypes=[zaaktype])
+        url = reverse("admin:catalogi_zaaktype_change", args=(zaaktype.pk,))
+
+        response = self.app.get(url)
+
+        # Verify that the publish button is visible
+        publish_button = response.html.find("input", {"name": "_publish"})
+        self.assertIsNotNone(publish_button)
+
+        form = response.forms["zaaktype_form"]
+
+        response = form.submit("_publish").follow()
+
+        zaaktype.refresh_from_db()
+        self.assertTrue(zaaktype.concept)
+
+        # Check that the error is shown on the page
+        error_message = response.html.find("li", {"class": "error"})
+        self.assertIn("should be published", error_message.text)
+
+        # Verify that the publish button is not on the page anymore
+        publish_button = response.html.find("input", {"name": "_publish"})
+        self.assertIsNotNone(publish_button)
+
+    def test_publish_zaaktype_related_to_concept_informatieobjecttype_fails(self, m):
+        mock_oas_get(m)
+        mock_resource_list(m, "procestypen")
+
+        zaaktype = ZaakTypeFactory.create(
+            concept=True,
+            zaaktype_omschrijving="test",
+            vertrouwelijkheidaanduiding="openbaar",
+            trefwoorden=["test"],
+            verantwoordingsrelatie=["bla"],
+        )
+        ZaakInformatieobjectTypeFactory.create(
+            informatieobjecttype__concept=True, zaaktype=zaaktype
+        )
+        url = reverse("admin:catalogi_zaaktype_change", args=(zaaktype.pk,))
+
+        response = self.app.get(url)
+
+        # Verify that the publish button is visible
+        publish_button = response.html.find("input", {"name": "_publish"})
+        self.assertIsNotNone(publish_button)
+
+        form = response.forms["zaaktype_form"]
+
+        response = form.submit("_publish").follow()
+
+        zaaktype.refresh_from_db()
+        self.assertTrue(zaaktype.concept)
+
+        # Check that the error is shown on the page
+        error_message = response.html.find("li", {"class": "error"})
+        self.assertIn("should be published", error_message.text)
+
+        # Verify that the publish button is not on the page anymore
+        publish_button = response.html.find("input", {"name": "_publish"})
+        self.assertIsNotNone(publish_button)
