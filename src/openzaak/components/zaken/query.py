ba50ed8c4bbf8c6688ed1923619d0ec5901e60b6
annashamray <anna.shamray@maykinmedia.nl>
2019-12-03 17:23:38 +0100

    :sparkles: add the support of external urls into AuthorizationsFilterMixin in zaken

--- src/openzaak/components/zaken/query.py
+++ src/openzaak/components/zaken/query.py
@@ -1,7 +1,9 @@
 from urllib.parse import urlparse
 
+from typing import Union
 from django.db import models
 from django.db.models import Case, IntegerField, Value, When
+from django.conf import settings
 
 from vng_api_common.constants import VertrouwelijkheidsAanduiding
 from vng_api_common.scopes import Scope
@@ -12,6 +14,64 @@ from openzaak.utils.query import BlockChangeMixin
 
 class AuthorizationsFilterMixin:
     authorizations_lookup = None
+
+    def get_zaaktype(self, authorization, local=True) -> Union[models.Model, str]:
+        if local:
+            zaaktype_path = urlparse(authorization.zaaktype).path
+            zaaktype = get_resource_for_path(zaaktype_path)
+        else:
+            zaaktype = authorization.zaaktype
+        return zaaktype
+
+    def filter_by_auth_query(self, scope, authorizations, local=True) -> models.QuerySet:
+        prefix = (
+            "" if not self.authorizations_lookup else f"{self.authorizations_lookup}__"
+        )
+        zaaktype_field = "zaaktype" if local else "_zaaktype_url"
+
+        # annotate the queryset so we can map a string value to a logical number
+        order_case = VertrouwelijkheidsAanduiding.get_order_expression(
+            f"{prefix}vertrouwelijkheidaanduiding"
+        )
+
+        # keep a list of allowed zaaktypen
+        zaaktypen = []
+        # build the case/when to map the max_vertrouwelijkheidaanduiding based
+        # on the ``zaaktype``
+        vertrouwelijkheidaanduiding_whens = []
+
+        for authorization in authorizations:
+            # test if this authorization has the scope that's needed
+            if not scope.is_contained_in(authorization.scopes):
+                continue
+
+            zaaktype = self.get_zaaktype(authorization, local)
+            zaaktypen.append(zaaktype)
+
+            # extract the order and map it to the database value
+            choice_item = VertrouwelijkheidsAanduiding.get_choice(
+                authorization.max_vertrouwelijkheidaanduiding
+            )
+            vertrouwelijkheidaanduiding_whens.append(
+                When(**{f"{prefix}{zaaktype_field}": zaaktype}, then=Value(choice_item.order))
+            )
+
+        # apply the order annnotation so we can filter later
+        annotations = {f"{prefix}_va_order": order_case}
+        # filtering:
+        # * only allow the white-listed zaaktypen, explicitly
+        # * apply the filtering to limit cases within case-types to the maximal
+        #   confidentiality level
+        filters = {
+            f"{prefix}{zaaktype_field}__in": zaaktypen,
+            f"{prefix}_va_order__lte": Case(
+                *vertrouwelijkheidaanduiding_whens, output_field=IntegerField()
+            ),
+        }
+
+        # bring it all together now to build the resulting queryset
+        queryset = self.annotate(**annotations).filter(**filters)
+        return queryset
 
     def filter_for_authorizations(
         self, scope: Scope, authorizations: models.QuerySet
@@ -40,55 +100,19 @@ class AuthorizationsFilterMixin:
         :return: a queryset of filtered results according to the
           authorizations provided
         """
-        # keep a list of allowed zaaktypen
-        zaaktypen = []
+        authorizations_local = []
+        authorizarions_external = []
+        for auth in authorizations:
+            zaaktype_host = urlparse(auth.zaaktype).hostname
+            if zaaktype_host in settings.ALLOWED_HOSTS:
+                authorizations_local.append(auth)
+            else:
+                authorizarions_external.append(auth)
 
-        prefix = (
-            "" if not self.authorizations_lookup else f"{self.authorizations_lookup}__"
-        )
+        queryset_local = self.filter_by_auth_query(scope, authorizations_local, local=True)
+        queryset_external = self.filter_by_auth_query(scope, authorizarions_external, local=False)
 
-        # annotate the queryset so we can map a string value to a logical number
-        order_case = VertrouwelijkheidsAanduiding.get_order_expression(
-            f"{prefix}vertrouwelijkheidaanduiding"
-        )
-
-        # build the case/when to map the max_vertrouwelijkheidaanduiding based
-        # on the ``zaaktype``
-        vertrouwelijkheidaanduiding_whens = []
-        for authorization in authorizations:
-            # test if this authorization has the scope that's needed
-            if not scope.is_contained_in(authorization.scopes):
-                continue
-
-            # this zaaktype is allowed
-            zaaktype_path = urlparse(authorization.zaaktype).path
-            zaaktype = get_resource_for_path(zaaktype_path)
-            zaaktypen.append(zaaktype)
-
-            # extract the order and map it to the database value
-            choice_item = VertrouwelijkheidsAanduiding.get_choice(
-                authorization.max_vertrouwelijkheidaanduiding
-            )
-            vertrouwelijkheidaanduiding_whens.append(
-                When(**{f"{prefix}zaaktype": zaaktype}, then=Value(choice_item.order))
-            )
-
-        # apply the order annnotation so we can filter later
-        annotations = {f"{prefix}_va_order": order_case}
-        # filtering:
-        # * only allow the white-listed zaaktypen, explicitly
-        # * apply the filtering to limit cases within case-types to the maximal
-        #   confidentiality level
-        filters = {
-            f"{prefix}zaaktype__in": zaaktypen,
-            f"{prefix}_va_order__lte": Case(
-                *vertrouwelijkheidaanduiding_whens, output_field=IntegerField()
-            ),
-        }
-
-        # bring it all together now to build the resulting queryset
-        queryset = self.annotate(**annotations).filter(**filters)
-        return queryset
+        return queryset_local.union(queryset_external)
 
 
 class ZaakQuerySet(AuthorizationsFilterMixin, models.QuerySet):
