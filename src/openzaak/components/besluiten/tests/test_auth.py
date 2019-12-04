0efcd069ffc953eb00a413f78a4cc660e722186a
annashamray <anna.shamray@maykinmedia.nl>
2019-12-04 17:03:40 +0100

    :white_check_mark: add tests for external url support in auth for besluiten

--- src/openzaak/components/besluiten/tests/test_auth.py
+++ src/openzaak/components/besluiten/tests/test_auth.py
@@ -1,6 +1,8 @@
 """
 Guarantee that the proper authorization amchinery is in place.
 """
+from django.test import override_settings, tag
+
 from rest_framework import status
 from rest_framework.test import APITestCase
 from vng_api_common.constants import ComponentTypes
@@ -15,6 +17,10 @@ from openzaak.utils.tests import JWTAuthMixin
 from ..api.scopes import SCOPE_BESLUITEN_AANMAKEN, SCOPE_BESLUITEN_ALLES_LEZEN
 from ..models import BesluitInformatieObject
 from .factories import BesluitFactory, BesluitInformatieObjectFactory
+
+BESLUITTYPE_EXTERNAL = (
+    "https://externe.catalogus.nl/api/v1/besluiten/b71f72ef-198d-44d8-af64-ae1932df830a"
+)
 
 
 class BesluitScopeForbiddenTests(AuthCheckMixin, APITestCase):
@@ -163,6 +169,156 @@ class BioReadTests(JWTAuthMixin, APITestCase):
         response2 = self.client.post(url2, data2)
 
         self.assertEqual(response1.status_code, status.HTTP_201_CREATED, response1.data)
+        self.assertEqual(
+            response2.status_code, status.HTTP_403_FORBIDDEN, response2.data
+        )
+
+
+@tag("external-urls")
+@override_settings(ALLOWED_HOSTS=["testserver"])
+class InternalBesluittypeScopeTests(JWTAuthMixin, APITestCase):
+    scopes = [SCOPE_BESLUITEN_ALLES_LEZEN]
+    component = ComponentTypes.brc
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.besluittype = BesluitTypeFactory.create()
+        super().setUpTestData()
+
+    def test_besluit_list(self):
+        BesluitFactory.create(besluittype=self.besluittype)
+        BesluitFactory.create(besluittype=BESLUITTYPE_EXTERNAL)
+        url = reverse("besluit-list")
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        results = response.data["results"]
+
+        self.assertEqual(len(results), 1)
+        self.assertEqual(
+            results[0]["besluittype"], f"http://testserver{reverse(self.besluittype)}"
+        )
+
+    def test_besluit_retrieve(self):
+        besluit1 = BesluitFactory.create(besluittype=self.besluittype)
+        besluit2 = BesluitFactory.create(besluittype=BESLUITTYPE_EXTERNAL)
+        url1 = reverse(besluit1)
+        url2 = reverse(besluit2)
+
+        response1 = self.client.get(url1)
+        response2 = self.client.get(url2)
+
+        self.assertEqual(response1.status_code, status.HTTP_200_OK)
+        self.assertEqual(response2.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_bio_list(self):
+        url = reverse(BesluitInformatieObject)
+        # must show up
+        bio1 = BesluitInformatieObjectFactory.create(
+            besluit__besluittype=self.besluittype,
+        )
+        # must not show up
+        BesluitInformatieObjectFactory.create(besluit__besluittype=BESLUITTYPE_EXTERNAL)
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response_data = response.json()
+
+        self.assertEqual(len(response_data), 1)
+
+        besluit_url = reverse(bio1.besluit)
+        self.assertEqual(response_data[0]["besluit"], f"http://testserver{besluit_url}")
+
+    def test_bio_retrieve(self):
+        bio1 = BesluitInformatieObjectFactory.create(
+            besluit__besluittype=self.besluittype
+        )
+        bio2 = BesluitInformatieObjectFactory.create(
+            besluit__besluittype=BESLUITTYPE_EXTERNAL
+        )
+
+        url1 = reverse(bio1)
+        url2 = reverse(bio2)
+
+        response1 = self.client.get(url1)
+        response2 = self.client.get(url2)
+
+        self.assertEqual(response1.status_code, status.HTTP_200_OK, response1.data)
+        self.assertEqual(
+            response2.status_code, status.HTTP_403_FORBIDDEN, response2.data
+        )
+
+
+@tag("external-urls")
+@override_settings(ALLOWED_HOSTS=["testserver"])
+class ExternalBesluittypeScopeTests(JWTAuthMixin, APITestCase):
+    scopes = [SCOPE_BESLUITEN_ALLES_LEZEN]
+    besluittype = BESLUITTYPE_EXTERNAL
+    component = ComponentTypes.brc
+
+    def test_besluit_list(self):
+        BesluitFactory.create(besluittype=self.besluittype)
+        BesluitFactory.create()
+        url = reverse("besluit-list")
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        results = response.data["results"]
+
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0]["besluittype"], self.besluittype)
+
+    def test_besluit_retrieve(self):
+        besluit1 = BesluitFactory.create(besluittype=self.besluittype)
+        besluit2 = BesluitFactory.create()
+        url1 = reverse(besluit1)
+        url2 = reverse(besluit2)
+
+        response1 = self.client.get(url1)
+        response2 = self.client.get(url2)
+
+        self.assertEqual(response1.status_code, status.HTTP_200_OK)
+        self.assertEqual(response2.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_bio_list(self):
+        url = reverse(BesluitInformatieObject)
+        # must show up
+        bio1 = BesluitInformatieObjectFactory.create(
+            besluit__besluittype=self.besluittype
+        )
+        # must not show up
+        BesluitInformatieObjectFactory.create()
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response_data = response.json()
+
+        self.assertEqual(len(response_data), 1)
+
+        besluit_url = reverse(bio1.besluit)
+        self.assertEqual(response_data[0]["besluit"], f"http://testserver{besluit_url}")
+
+    def test_bio_retrieve(self):
+        bio1 = BesluitInformatieObjectFactory.create(
+            besluit__besluittype=self.besluittype
+        )
+        bio2 = BesluitInformatieObjectFactory.create()
+
+        url1 = reverse(bio1)
+        url2 = reverse(bio2)
+
+        response1 = self.client.get(url1)
+        response2 = self.client.get(url2)
+
+        self.assertEqual(response1.status_code, status.HTTP_200_OK, response1.data)
         self.assertEqual(
             response2.status_code, status.HTTP_403_FORBIDDEN, response2.data
         )
