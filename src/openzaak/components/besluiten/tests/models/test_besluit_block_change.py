655a0115ec46bccdf70fde739b82237e360ab921
annashamray <anna.shamray@maykinmedia.nl>
2019-12-06 11:48:10 +0100

    :white_check_mark: test blocking changes in Beluit query

---
+++ src/openzaak/components/besluiten/tests/models/test_besluit_block_change.py
@@ -0,0 +1,35 @@
+import uuid
+
+from django.test import TestCase
+
+from openzaak.components.catalogi.tests.factories import BesluitTypeFactory
+from openzaak.utils.query import QueryBlocked
+
+from ...models import Besluit
+from ..factories import BesluitFactory
+
+
+class BlockChangeTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+
+        cls.besluit = BesluitFactory.create()
+
+    def test_update(self):
+        self.assertRaises(QueryBlocked, Besluit.objects.update, uuid=uuid.uuid4())
+
+    def test_delete(self):
+        self.assertRaises(QueryBlocked, Besluit.objects.all().delete)
+
+    def test_bulk_update(self):
+        self.besluit.uuid = uuid.uuid4()
+        self.assertRaises(
+            QueryBlocked, Besluit.objects.bulk_update, [self.besluit], fields=["uuid"],
+        )
+
+    def test_bulk_create(self):
+        besluittype = BesluitTypeFactory.create()
+        besluit = BesluitFactory.build(besluittype=besluittype)
+
+        self.assertRaises(QueryBlocked, Besluit.objects.bulk_create, [besluit])
