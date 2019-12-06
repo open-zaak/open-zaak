3e809f2955751380b108c13a8f5e9920dbecfe71
annashamray <anna.shamray@maykinmedia.nl>
2019-12-06 18:12:45 +0100

    :bug: add support for external zaak to validator

--- src/openzaak/components/besluiten/api/validators.py
+++ src/openzaak/components/besluiten/api/validators.py
@@ -5,7 +5,7 @@ from vng_api_common.validators import (
     UniekeIdentificatieValidator as _UniekeIdentificatieValidator,
 )
 
-from openzaak.components.documenten.models import EnkelvoudigInformatieObject
+from openzaak.loaders import AuthorizedRequestsLoader
 
 
 class UniekeIdentificatieValidator(_UniekeIdentificatieValidator):
@@ -24,12 +24,35 @@ class BesluittypeZaaktypeValidator:
     code = "zaaktype-mismatch"
     message = _("De referentie hoort niet bij het zaaktype van de zaak.")
 
+    def set_context(self, serializer):
+        self.instance = getattr(serializer, "instance", None)
+
     def __call__(self, attrs):
-        besluittype = attrs.get("besluittype")
-        zaak = attrs.get("zaak")
+        besluittype = attrs.get("besluittype") or self.instance.besluittype
+        zaak = attrs.get("zaak") or getattr(self.instance, "zaak", None)
 
         if not zaak:
             return
 
-        if not besluittype.zaaktypen.filter(uuid=zaak.zaaktype.uuid).exists():
-            raise serializers.ValidationError(self.message, code=self.code)
+        zaaktype = zaak.zaaktype
+
+        if bool(zaaktype.pk) != bool(besluittype.pk):
+            msg_diff = _(
+                "Het besluittype en het zaaktype van de/het zaak moeten tot dezelfde catalogus behoren."
+            )
+            raise serializers.ValidationError(msg_diff, code=self.code)
+
+        # local zaaktype/besluittype
+        if besluittype.pk:
+            if not besluittype.zaaktypen.filter(uuid=zaaktype.uuid).exists():
+                raise serializers.ValidationError(self.message, code=self.code)
+
+        # external zaaktype/besluittype - workaround since loose-fk field doesn't support m2m relations
+        else:
+            besluittype_url = besluittype._loose_fk_data["url"]
+            zaaktype_url = zaaktype._loose_fk_data["url"]
+            besluittype_data = AuthorizedRequestsLoader.fetch_object(
+                besluittype_url, do_underscoreize=False
+            )
+            if zaaktype_url not in besluittype_data.get("zaaktypen", []):
+                raise serializers.ValidationError(self.message, code=self.code)
