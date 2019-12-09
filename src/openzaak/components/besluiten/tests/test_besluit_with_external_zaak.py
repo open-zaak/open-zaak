c894349a1c0ca48bf0bdd250b82710a72815a795
annashamray <anna.shamray@maykinmedia.nl>
2019-12-09 12:04:48 +0100

    :white_check_mark: test update and delete for besluit with external zaak

--- src/openzaak/components/besluiten/tests/test_besluit_with_external_zaak.py
+++ src/openzaak/components/besluiten/tests/test_besluit_with_external_zaak.py
@@ -17,6 +17,7 @@ from openzaak.utils.tests import JWTAuthMixin
 
 from ..constants import VervalRedenen
 from ..models import Besluit
+from .factories import BesluitFactory
 from .utils import get_operation_url
 
 
@@ -24,6 +25,23 @@ from .utils import get_operation_url
 @override_settings(ALLOWED_HOSTS=["testserver"])
 class BesluitCreateExternalzaakTests(TypeCheckMixin, JWTAuthMixin, APITestCase):
     heeft_alle_autorisaties = True
+
+    def _create_besluit(self, zaak: str) -> Besluit:
+        besluittype = BesluitTypeFactory.create(concept=False, zaaktypen=[])
+        zaaktype = besluittype.zaaktypen.get()
+        zaaktype_url = f"http://testserver{reverse(zaaktype)}"
+        zaakbesluit_data = get_zaakbesluit_response(zaak)
+
+        # create besluit
+        with requests_mock.Mocker(real_http=True) as m:
+            m.get(zaak, json=get_zaak_response(zaak, zaaktype_url))
+            m.post(f"{zaak}/besluiten", json=zaakbesluit_data)
+            besluit = BesluitFactory.create(
+                zaak=zaak,
+                besluittype=besluittype,
+                _zaakbesluit_url=zaakbesluit_data["url"],
+            )
+        return besluit
 
     def test_create_with_external_zaak(self):
         zaak = "https://externe.catalogus.nl/api/v1/zaken/1c8e36be-338c-4c07-ac5e-1adf55bec04a"
@@ -60,7 +78,11 @@ class BesluitCreateExternalzaakTests(TypeCheckMixin, JWTAuthMixin, APITestCase):
         besluit_url = f"http://testserver{reverse(besluit)}"
         self.assertEqual(besluit._zaakbesluit_url, zaakbesluit_data["url"])
 
-        history_post = [req for req in m.request_history if req.method == "POST" and req.url == f"{zaak}/besluiten"]
+        history_post = [
+            req
+            for req in m.request_history
+            if req.method == "POST" and req.url == f"{zaak}/besluiten"
+        ]
         self.assertEqual(len(history_post), 1)
         self.assertEqual(history_post[0].json(), {"besluit": besluit_url})
 
@@ -77,7 +99,7 @@ class BesluitCreateExternalzaakTests(TypeCheckMixin, JWTAuthMixin, APITestCase):
 
         with requests_mock.Mocker(real_http=True) as m:
             m.get(zaak, json=get_zaak_response(zaak, zaaktype_url))
-            m.post(f"{zaak}/besluiten", status_code=404, text='Not Found')
+            m.post(f"{zaak}/besluiten", status_code=404, text="Not Found")
 
             response = self.client.post(
                 url,
@@ -94,69 +116,126 @@ class BesluitCreateExternalzaakTests(TypeCheckMixin, JWTAuthMixin, APITestCase):
                 },
             )
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.data)
+        self.assertEqual(
+            response.status_code, status.HTTP_400_BAD_REQUEST, response.data
+        )
 
         error = get_validation_errors(response, "zaak")
-        self.assertEqual(error['code'], 'pending-relations')
+        self.assertEqual(error["code"], "pending-relations")
 
     def test_update_external_zaak(self):
-        besluittype = BesluitTypeFactory.create(concept=False)
-        zaaktype = ZaakTypeFactory.create(
-            concept=False, catalogus=besluittype.catalogus
-        )
-        zaaktype.besluittypen.add(besluittype)
+        zaak_old = "https://externe.catalogus.nl/api/v1/zaken/old"
+        besluit = self._create_besluit(zaak_old)
+        zaaktype = besluit.besluittype.zaaktypen.get()
         zaaktype_url = f"http://testserver{reverse(zaaktype)}"
-        zaak_old = "https://externe.catalogus.nl/api/v1/zaken/1c8e36be-338c-4c07-ac5e-1adf55bec04a"
-        zaakbesluit_old_data = get_zaakbesluit_response(zaak_old)
-        url = get_operation_url("besluit_create")
-
-        # create besluit
-        with requests_mock.Mocker(real_http=True) as m:
-            m.get(zaak_old, json=get_zaak_response(zaak_old, zaaktype_url))
-            m.post(f"{zaak_old}/besluiten", json=zaakbesluit_old_data)
-
-            response_create = self.client.post(
-                url,
-                {
-                    "verantwoordelijke_organisatie": "517439943",
-                    "identificatie": "123123",
-                    "besluittype": f"http://testserver{reverse(besluittype)}",
-                    "datum": "2018-09-06",
-                    "toelichting": "Vergunning verleend.",
-                    "ingangsdatum": "2018-10-01",
-                    "vervaldatum": "2018-11-01",
-                    "vervalreden": VervalRedenen.tijdelijk,
-                    "zaak": zaak_old,
-                },
-            )
-        self.assertEqual(response_create.status_code, status.HTTP_201_CREATED, response_create.data)
-
-        besluit = Besluit.objects.get()
-        besluit_url = f"http://testserver{reverse(besluit)}"
-        zaak_new = "https://externe.catalogus.nl/api/v1/zaken/19b702ce-1387-42a3-87d9-b070e8c3f43d"
-        zaakbesluit_new_data = get_zaakbesluit_response(zaak_new)
+        old_zaakbesluit_url = besluit._zaakbesluit_url
 
         # update zaak in the besluit
+        zaak_new = "https://externe.catalogus.nl/api/v1/zaken/new"
+        zaakbesluit_new_data = get_zaakbesluit_response(zaak_new)
+        besluit_url = f"http://testserver{reverse(besluit)}"
+
         with requests_mock.Mocker(real_http=True) as m:
             m.get(zaak_old, json=get_zaak_response(zaak_old, zaaktype_url))
             m.get(zaak_new, json=get_zaak_response(zaak_new, zaaktype_url))
-            m.delete(besluit._zaakbesluit_url)
+            m.delete(old_zaakbesluit_url)
             m.post(f"{zaak_new}/besluiten", json=zaakbesluit_new_data)
 
-            response_update = self.client.patch(besluit_url, {"zaak": zaak_new})
+            response = self.client.patch(besluit_url, {"zaak": zaak_new})
 
-        self.assertEqual(response_update.status_code, status.HTTP_200_OK, response_update.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
 
+        besluit.refresh_from_db()
 
+        self.assertEqual(besluit._previous_zaak_url, zaak_old)
+        self.assertEqual(besluit._zaak_url, zaak_new)
+        self.assertEqual(besluit._zaakbesluit_url, zaakbesluit_new_data["url"])
 
-    def test_update_external_zaak_fail_update_zaakbesluit(self):
-        # TODO
-        pass
+        history_post = [
+            req
+            for req in m.request_history
+            if req.method == "POST" and req.url == f"{zaak_new}/besluiten"
+        ]
+        self.assertEqual(len(history_post), 1)
+        self.assertEqual(history_post[0].json(), {"besluit": besluit_url})
+
+        history_delete = [
+            req
+            for req in m.request_history
+            if req.method == "DELETE" and req.url == old_zaakbesluit_url
+        ]
+        self.assertEqual(len(history_delete), 1)
+
+    def test_update_external_zaak_fail_delete_zaakbesluit(self):
+        zaak_old = "https://externe.catalogus.nl/api/v1/zaken/old"
+        besluit = self._create_besluit(zaak_old)
+        zaaktype = besluit.besluittype.zaaktypen.get()
+        zaaktype_url = f"http://testserver{reverse(zaaktype)}"
+        old_zaakbesluit_url = besluit._zaakbesluit_url
+
+        # update zaak in the besluit
+        zaak_new = "https://externe.catalogus.nl/api/v1/zaken/new"
+        zaakbesluit_new_data = get_zaakbesluit_response(zaak_new)
+        besluit_url = f"http://testserver{reverse(besluit)}"
+
+        with requests_mock.Mocker(real_http=True) as m:
+            m.get(zaak_old, json=get_zaak_response(zaak_old, zaaktype_url))
+            m.get(zaak_new, json=get_zaak_response(zaak_new, zaaktype_url))
+            m.delete(old_zaakbesluit_url, status_code=404, text="not found")
+            m.post(f"{zaak_new}/besluiten", json=zaakbesluit_new_data)
+
+            response = self.client.patch(besluit_url, {"zaak": zaak_new})
+
+        self.assertEqual(
+            response.status_code, status.HTTP_400_BAD_REQUEST, response.data
+        )
+
+        error = get_validation_errors(response, "zaak")
+        self.assertEqual(error["code"], "pending-relations")
 
     def test_delete_with_external_zaak(self):
-        # TODO
-        pass
+        zaak = "https://externe.catalogus.nl/api/v1/zaken/1c8e36be-338c-4c07-ac5e-1adf55bec04a"
+        besluit = self._create_besluit(zaak)
+        besluit_url = f"http://testserver{reverse(besluit)}"
+        zaakbesluit_url = besluit._zaakbesluit_url
+        zaaktype = besluit.besluittype.zaaktypen.get()
+        zaaktype_url = f"http://testserver{reverse(zaaktype)}"
+
+        with requests_mock.Mocker(real_http=True) as m:
+            m.get(zaak, json=get_zaak_response(zaak, zaaktype_url))
+            m.delete(zaakbesluit_url)
+
+            response = self.client.delete(besluit_url)
+
+        self.assertEqual(
+            response.status_code, status.HTTP_204_NO_CONTENT, response.data
+        )
+        self.assertEqual(Besluit.objects.count(), 0)
+
+        history_delete = [
+            req
+            for req in m.request_history
+            if req.method == "DELETE" and req.url == zaakbesluit_url
+        ]
+        self.assertEqual(len(history_delete), 1)
 
     def test_delete_with_external_zaak_fail_delete_zaakbesluit(self):
-        # TODO
-        pass
+        zaak = "https://externe.catalogus.nl/api/v1/zaken/1c8e36be-338c-4c07-ac5e-1adf55bec04a"
+        besluit = self._create_besluit(zaak)
+        besluit_url = f"http://testserver{reverse(besluit)}"
+        zaakbesluit_url = besluit._zaakbesluit_url
+        zaaktype = besluit.besluittype.zaaktypen.get()
+        zaaktype_url = f"http://testserver{reverse(zaaktype)}"
+
+        with requests_mock.Mocker(real_http=True) as m:
+            m.get(zaak, json=get_zaak_response(zaak, zaaktype_url))
+            m.delete(zaakbesluit_url, status_code=404, text="Not found")
+
+            response = self.client.delete(besluit_url)
+
+        self.assertEqual(
+            response.status_code, status.HTTP_400_BAD_REQUEST, response.data
+        )
+
+        error = get_validation_errors(response, "zaak")
+        self.assertEqual(error["code"], "pending-relations")
