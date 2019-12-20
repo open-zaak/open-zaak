94cd576613e7b1aeece757396a32992048e121d5
annashamray <anna.shamray@maykinmedia.nl>
2019-12-20 12:43:28 +0100

    test zaaktype mismatch with external resultaattype

--- src/openzaak/components/zaken/tests/test_resultaten.py
+++ src/openzaak/components/zaken/tests/test_resultaten.py
@@ -8,12 +8,7 @@ from vng_api_common.tests import get_validation_errors, reverse
 from openzaak.utils.tests import JWTAuthMixin
 
 from .factories import ZaakFactory
-from .utils import (
-    get_catalogus_response,
-    get_operation_url,
-    get_resultaattype_response,
-    get_zaaktype_response,
-)
+from .utils import get_operation_url, get_resultaattype_response, get_zaaktype_response
 
 
 @tag("external-urls")
@@ -22,7 +17,7 @@ class ResultaatCreateExternalURLsTests(JWTAuthMixin, APITestCase):
     heeft_alle_autorisaties = True
     list_url = get_operation_url("resultaat_create")
 
-    def test_create_external_resultaat(self):
+    def test_create_external_resultaattype(self):
         catalogus = "https://externe.catalogus.nl/api/v1/catalogussen/1c8e36be-338c-4c07-ac5e-1adf55bec04a"
         zaaktype = "https://externe.catalogus.nl/api/v1/zaaktypen/b71f72ef-198d-44d8-af64-ae1932df830a"
         resultaattype = "https://externe.catalogus.nl/api/v1/resultaten/7a3e4a22-d789-4381-939b-401dbce29426"
@@ -47,7 +42,7 @@ class ResultaatCreateExternalURLsTests(JWTAuthMixin, APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data)
 
-    def test_create_external_zaaktype_fail_bad_url(self):
+    def test_create_external_resultaattype_fail_bad_url(self):
         zaak = ZaakFactory()
         zaak_url = reverse(zaak)
 
@@ -67,7 +62,7 @@ class ResultaatCreateExternalURLsTests(JWTAuthMixin, APITestCase):
         error = get_validation_errors(response, "resultaattype")
         self.assertEqual(error["code"], "bad-url")
 
-    def test_create_external_zaaktype_fail_not_json_url(self):
+    def test_create_external_resultaattype_fail_not_json_url(self):
         zaak = ZaakFactory()
         zaak_url = reverse(zaak)
 
@@ -83,7 +78,7 @@ class ResultaatCreateExternalURLsTests(JWTAuthMixin, APITestCase):
         error = get_validation_errors(response, "resultaattype")
         self.assertEqual(error["code"], "invalid-resource")
 
-    def test_create_external_zaaktype_fail_invalid_schema(self):
+    def test_create_external_resultaattype_fail_invalid_schema(self):
         catalogus = "https://externe.catalogus.nl/api/v1/catalogussen/1c8e36be-338c-4c07-ac5e-1adf55bec04a"
         zaaktype = "https://externe.catalogus.nl/api/v1/zaaktypen/b71f72ef-198d-44d8-af64-ae1932df830a"
         resultaattype = "https://externe.catalogus.nl/api/v1/resultaten/7a3e4a22-d789-4381-939b-401dbce29426"
@@ -116,3 +111,33 @@ class ResultaatCreateExternalURLsTests(JWTAuthMixin, APITestCase):
 
         error = get_validation_errors(response, "resultaattype")
         self.assertEqual(error["code"], "invalid-resource")
+
+    def test_create_external_resultaattype_fail_zaaktype_mismatch(self):
+        catalogus = "https://externe.catalogus.nl/api/v1/catalogussen/1c8e36be-338c-4c07-ac5e-1adf55bec04a"
+        zaaktype1 = "https://externe.catalogus.nl/api/v1/zaaktypen/b71f72ef-198d-44d8-af64-ae1932df830a"
+        zaaktype2 = "https://externe.catalogus.nl/api/v1/zaaktypen/b923543f-97aa-4a55-8c20-889b5906cf75"
+        resultaattype = "https://externe.catalogus.nl/api/v1/resultaten/7a3e4a22-d789-4381-939b-401dbce29426"
+
+        zaak = ZaakFactory(zaaktype=zaaktype1)
+        zaak_url = reverse(zaak)
+
+        with requests_mock.Mocker(real_http=True) as m:
+            m.get(zaaktype1, json=get_zaaktype_response(catalogus, zaaktype1))
+            m.get(zaaktype2, json=get_zaaktype_response(catalogus, zaaktype2))
+            m.get(
+                resultaattype, json=get_resultaattype_response(resultaattype, zaaktype2)
+            )
+
+            response = self.client.post(
+                self.list_url,
+                {
+                    "zaak": f"http://testserver{zaak_url}",
+                    "resultaattype": resultaattype,
+                    "toelichting": "some desc",
+                },
+            )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        error = get_validation_errors(response, "nonFieldErrors")
+        self.assertEqual(error["code"], "zaaktype-mismatch")
