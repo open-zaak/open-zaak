32003612c437cfcf00e8acc4c3dd86d45ba25c18
annashamray <anna.shamray@maykinmedia.nl>
2019-12-23 14:29:33 +0100

    clean migrations after one more rebase

--- src/openzaak/components/zaken/migrations/0026_auto_20191219_1657.py
+++ src/openzaak/components/zaken/migrations/0026_auto_20191219_1657.py
@@ -8,7 +8,7 @@ import django_loose_fk.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("zaken", "0023_auto_20191219_1650"),
+        ("zaken", "0025_auto_20191219_1650"),
     ]
 
     operations = [
