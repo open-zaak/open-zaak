e35c0b2f397d90fe9e21bf7fb5ba44c334463eae
annashamray <anna.shamray@maykinmedia.nl>
2019-12-24 11:49:22 +0100

    add external url support to oio filter

--- src/openzaak/components/documenten/api/filters.py
+++ src/openzaak/components/documenten/api/filters.py
@@ -1,15 +1,12 @@
 from urllib.parse import urlparse
 
-from django.db.models import ObjectDoesNotExist
 from django.utils.translation import ugettext_lazy as _
 
 from django_filters import rest_framework as filters
+from django_loose_fk.filters import FkOrUrlFieldFilter
 from vng_api_common.filters import URLModelChoiceFilter
 from vng_api_common.filtersets import FilterSet
-from vng_api_common.utils import get_help_text, get_viewset_for_path
-
-from openzaak.components.besluiten.models import Besluit
-from openzaak.components.zaken.models import Zaak
+from vng_api_common.utils import get_help_text
 
 from ..models import (
     EnkelvoudigInformatieObject,
@@ -48,26 +45,13 @@ class GebruiksrechtenFilter(FilterSet):
         }
 
 
-def object_queryset(request):
-    object_value = request.query_params.get("object", "")
-    object_path = urlparse(object_value).path
-
-    # get main_object data formatted by serializer
-    try:
-        viewset = get_viewset_for_path(object_path)
-    except ObjectDoesNotExist:
-        # the exception is raised in URLModelChoiceField.to_python method
-        return ObjectInformatieObject.objects.all()
-    model = viewset.get_queryset().model
-    return model._default_manager.all()
-
-
-class ObjectFilter(URLModelChoiceFilter):
+class ObjectFilter(FkOrUrlFieldFilter):
     def filter(self, qs, value):
-        if isinstance(value, Zaak):
-            self.field_name = "zaak"
-        elif isinstance(value, Besluit):
+        path = urlparse(value).path
+        if "/besluiten/" in path:
             self.field_name = "besluit"
+        else:
+            self.field_name = "zaak"
 
         return super().filter(qs, value)
 
@@ -81,7 +65,7 @@ class ObjectInformatieObjectFilter(FilterSet):
         ),
     )
     object = ObjectFilter(
-        queryset=object_queryset,
+        queryset=ObjectInformatieObject.objects.all(),
         help_text=_(
             "URL-referentie naar het gerelateerde OBJECT (in deze of een andere API)."
         ),
