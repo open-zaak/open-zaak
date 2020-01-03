4c83bd39e21bac3e504ff56be18eb62af80c273e
Sergei Maertens <sergei@maykinmedia.nl>
2020-01-03 13:06:08 +0100

    :sparkles: Implement admin action to resend failed notifications

--- src/openzaak/notifications/admin.py
+++ src/openzaak/notifications/admin.py
@@ -1,8 +1,38 @@
+import logging
+
 from django.contrib import admin
+from django.db import transaction
+from django.db.models import QuerySet
+from django.http import HttpRequest
 from django.urls import reverse
 from django.utils.html import format_html
+from django.utils.translation import ugettext_lazy as _
 
 from .models import FailedNotification
+from .resend import ResendFailure, resend_notification
+
+logger = logging.getLogger(__name__)
+
+
+def resend_notifications(
+    modeladmin: admin.ModelAdmin, request: HttpRequest, queryset: QuerySet
+) -> None:
+    for failed in queryset:
+        if failed.retried_at is not None:
+            logger.info("Not resending already re-sent notification %d", failed.pk)
+            continue
+
+        with transaction.atomic():
+            try:
+                resend_notification(failed)
+            except ResendFailure:
+                continue
+            except Exception:
+                logger.exception("Resend for %d failed", failed.pk)
+                continue
+
+
+resend_notifications.short_description = _("Resend %(verbose_name_plural)s")
 
 
 @admin.register(FailedNotification)
@@ -11,6 +41,7 @@ class FailedNotificationAdmin(admin.ModelAdmin):
     list_filter = ("retried_at",)
     date_hierarchy = "create_datetime"
     search_fields = ("message__kanaal",)
+    actions = [resend_notifications]
 
     def kanaal(self, obj) -> str:
         return obj.message["kanaal"]
