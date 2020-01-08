91ceab43c2eadab3aaae849ee4f31d74f77b016f
Sergei Maertens <sergei@maykinmedia.nl>
2020-01-08 11:52:52 +0100

    :whale: Pin open zaak image version

--- deployment/single-server/gemeente-buren.sh
+++ deployment/single-server/gemeente-buren.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ansible-playbook open-zaak.yml -i gemeente-buren-hosts -e "certbot_create_if_missing=false"
+ansible-playbook open-zaak.yml \
+    -i gemeente-buren-hosts \
+    -e "certbot_create_if_missing=false openzaak_version=20200108"
