d5edf48fd054b727d082cb6ea79d2ef7a59c6502
Sergei Maertens <sergei@maykinmedia.nl>
2020-01-08 15:00:25 +0100

    :rocket: Update hostnames

--- deployment/single-server/gemeente-buren-notificaties.sh
+++ deployment/single-server/gemeente-buren-notificaties.sh
@@ -2,7 +2,7 @@
 
 ansible-playbook open-notificaties.yml \
     -i gemeente-buren-hosts \
-    --limit webref01-notificaties \
+    --limit open-notificaties.buren.lan \
     -e "certbot_create_if_missing=false opennotificaties_version=latest app_db_name=opennotificaties-staging app_db_user=opennotificaties-staging" \
     -c local \
     --become \
--- deployment/single-server/gemeente-buren.sh
+++ deployment/single-server/gemeente-buren.sh
@@ -2,8 +2,8 @@
 
 ansible-playbook open-zaak.yml \
     -i gemeente-buren-hosts \
-    --limit webref01 \
-    -e "certbot_create_if_missing=false openzaak_domain=open-zaak.buren.lan openzaak_version=20200108 app_db_name=openzaak-staging app_db_user=openzaak-staging" \
+    --limit open-zaak.buren.lan \
+    -e "certbot_create_if_missing=false openzaak_version=20200108 app_db_name=openzaak-staging app_db_user=openzaak-staging" \
     -c local \
     --become \
     --become-method=sudo \
