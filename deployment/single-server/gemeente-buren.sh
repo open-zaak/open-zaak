2c362d1d781d2090c9b794eedfb5266d52fcc30c
Sergei Maertens <sergei@maykinmedia.nl>
2020-01-08 11:56:25 +0100

    :rocket: Deploy from target machine

--- deployment/single-server/gemeente-buren.sh
+++ deployment/single-server/gemeente-buren.sh
@@ -2,4 +2,9 @@
 
 ansible-playbook open-zaak.yml \
     -i gemeente-buren-hosts \
-    -e "certbot_create_if_missing=false openzaak_version=20200108"
+    -e "certbot_create_if_missing=false openzaak_version=20200108" \
+    -c local \
+    --become \
+    --become-method=sudo \
+    --ask-vault-pass
+
