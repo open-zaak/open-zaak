5966d9f0fd48cef55e3addd1f7f2bcf304d3a028
Sergei Maertens <sergei@maykinmedia.nl>
2020-01-08 14:03:26 +0100

    :rocket: Prep Open Notificaties deploy

---
+++ deployment/single-server/gemeente-buren-notificaties.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ansible-playbook open-zaak.yml \
+    -i gemeente-buren-hosts \
+    --limit webref01-notificaties \
+    -e "certbot_create_if_missing=false opennotificaties_version=latest app_db_name=opennotificaties-staging app_db_user=opennotificaties-staging" \
+    -c local \
+    --become \
+    --become-method=sudo \
+    --ask-become-pass
+
--- deployment/single-server/gemeente-buren.sh
+++ deployment/single-server/gemeente-buren.sh
@@ -2,6 +2,7 @@
 
 ansible-playbook open-zaak.yml \
     -i gemeente-buren-hosts \
+    --limit webref01 \
     -e "certbot_create_if_missing=false openzaak_version=20200108 app_db_name=openzaak-staging app_db_user=openzaak-staging" \
     -c local \
     --become \
