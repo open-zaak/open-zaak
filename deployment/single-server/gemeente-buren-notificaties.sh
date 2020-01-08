2a29a6f3ce40d8ac05f937065b5bc7d08b2efddf
Sergei Maertens <sergei@maykinmedia.nl>
2020-01-08 14:10:25 +0100

    :bug: Deploying the correct playbook would be nice...

--- deployment/single-server/gemeente-buren-notificaties.sh
+++ deployment/single-server/gemeente-buren-notificaties.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ansible-playbook open-zaak.yml \
+ansible-playbook open-notificaties.yml \
     -i gemeente-buren-hosts \
     --limit webref01-notificaties \
     -e "certbot_create_if_missing=false opennotificaties_version=latest app_db_name=opennotificaties-staging app_db_user=opennotificaties-staging" \
