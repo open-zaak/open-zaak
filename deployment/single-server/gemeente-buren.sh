e45909ba11e75c5c7373cb1d5ef1f4dd8e5066f8
Sergei Maertens <sergei@maykinmedia.nl>
2020-01-08 14:44:19 +0100

    :rocket: Set up correct domain

--- deployment/single-server/gemeente-buren.sh
+++ deployment/single-server/gemeente-buren.sh
@@ -3,7 +3,7 @@
 ansible-playbook open-zaak.yml \
     -i gemeente-buren-hosts \
     --limit webref01 \
-    -e "certbot_create_if_missing=false openzaak_version=20200108 app_db_name=openzaak-staging app_db_user=openzaak-staging" \
+    -e "certbot_create_if_missing=false openzaak_domain=open-zaak.buren.lan openzaak_version=20200108 app_db_name=openzaak-staging app_db_user=openzaak-staging" \
     -c local \
     --become \
     --become-method=sudo \
