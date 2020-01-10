a322ad43ccb8cd7e64a273c547e8277dcb09634f
annashamray <anna.shamray@maykinmedia.nl>
2020-01-10 12:35:19 +0100

    add list_filter, search_fields and ordering to zaken admin

--- src/openzaak/components/zaken/admin/zaken.py
+++ src/openzaak/components/zaken/admin/zaken.py
@@ -25,14 +25,23 @@ from ..models import (
 class StatusAdmin(AuditTrailAdminMixin, UUIDAdminMixin, admin.ModelAdmin):
     list_display = ["zaak", "datum_status_gezet"]
     list_select_related = ["zaak"]
+    list_filter = ("datum_status_gezet",)
+    search_fields = (
+        "uuid",
+        "statustoelichting",
+    )
+    ordering = ("datum_status_gezet",)
     raw_id_fields = ["zaak", "_statustype"]
     viewset = "openzaak.components.zaken.api.viewsets.StatusViewSet"
 
 
 @admin.register(ZaakObject)
 class ZaakObjectAdmin(AuditTrailAdminMixin, UUIDAdminMixin, admin.ModelAdmin):
-    list_display = ["zaak", "object", "relatieomschrijving"]
+    list_display = ["zaak", "object_type", "object", "relatieomschrijving"]
     list_select_related = ["zaak"]
+    list_filter = ("object_type",)
+    search_fields = ("uuid", "object", "relatieomschrijving")
+    ordering = ("object_type", "object")
     raw_id_fields = ["zaak"]
     viewset = "openzaak.components.zaken.api.viewsets.ZaakObjectViewSet"
 
@@ -41,6 +50,9 @@ class ZaakObjectAdmin(AuditTrailAdminMixin, UUIDAdminMixin, admin.ModelAdmin):
 class KlantContactAdmin(AuditTrailAdminMixin, UUIDAdminMixin, admin.ModelAdmin):
     list_display = ["zaak", "identificatie", "datumtijd", "kanaal"]
     list_select_related = ["zaak"]
+    list_filter = ("datumtijd",)
+    search_fields = ("uuid", "identificatie", "toelichting", "kanaal")
+    ordering = ("identificatie", "datumtijd")
     raw_id_fields = ["zaak"]
     viewset = "openzaak.components.zaken.api.viewsets.KlantContactViewSet"
 
@@ -49,7 +61,10 @@ class KlantContactAdmin(AuditTrailAdminMixin, UUIDAdminMixin, admin.ModelAdmin):
 class ZaakEigenschapAdmin(AuditTrailAdminMixin, UUIDAdminMixin, admin.ModelAdmin):
     list_display = ["zaak", "_eigenschap", "_eigenschap_url", "waarde"]
     list_select_related = ["zaak"]
-    raw_id_fields = ["zaak"]
+    list_filter = ("_naam",)
+    search_fields = ("uuid", "_naam", "waarde")
+    ordering = ("zaak", "_eigenschap", "_eigenschap_url")
+    raw_id_fields = ["zaak", "_eigenschap"]
     viewset = "openzaak.components.zaken.api.viewsets.ZaakEigenschapViewSet"
 
 
@@ -57,6 +72,9 @@ class ZaakEigenschapAdmin(AuditTrailAdminMixin, UUIDAdminMixin, admin.ModelAdmin
 class ZaakInformatieObjectAdmin(AuditTrailAdminMixin, UUIDAdminMixin, admin.ModelAdmin):
     list_display = ["zaak", "_informatieobject", "_informatieobject_url"]
     list_select_related = ["zaak", "_informatieobject"]
+    list_filter = ("aard_relatie",)
+    search_fields = ("uuid", "zaak", "_informatieobject", "_informatieobject_url")
+    ordering = ("zaak", "_informatieobject", "_informatieobject_url")
     raw_id_fields = ["zaak", "_informatieobject"]
     viewset = "openzaak.components.zaken.api.viewsets.ZaakInformatieObjectViewSet"
 
@@ -64,7 +82,9 @@ class ZaakInformatieObjectAdmin(AuditTrailAdminMixin, UUIDAdminMixin, admin.Mode
 @admin.register(Resultaat)
 class ResultaatAdmin(AuditTrailAdminMixin, UUIDAdminMixin, admin.ModelAdmin):
     list_display = ["zaak", "toelichting"]
-    list_select_related = ["zaak"]
+    list_select_related = ["zaak", "_resultaattype"]
+    search_fields = ("uuid", "toelichting", "_resultaattype", "_resultaattype_url")
+    ordering = ("zaak",)
     raw_id_fields = ["zaak", "_resultaattype"]
     viewset = "openzaak.components.zaken.api.viewsets.ResultaatViewSet"
 
@@ -73,6 +93,14 @@ class ResultaatAdmin(AuditTrailAdminMixin, UUIDAdminMixin, admin.ModelAdmin):
 class RolAdmin(AuditTrailAdminMixin, UUIDAdminMixin, admin.ModelAdmin):
     list_display = ["zaak", "betrokkene", "betrokkene_type"]
     list_select_related = ["zaak"]
+    list_filter = ("betrokkene_type", "indicatie_machtiging", "registratiedatum")
+    search_fields = (
+        "uuid",
+        "betrokkene",
+        "omschrijving",
+        "roltoelichting",
+    )
+    ordering = ("registratiedatum", "betrokkene")
     raw_id_fields = ["zaak", "_roltype"]
     viewset = "openzaak.components.zaken.api.viewsets.RolViewSet"
 
@@ -80,6 +108,9 @@ class RolAdmin(AuditTrailAdminMixin, UUIDAdminMixin, admin.ModelAdmin):
 @admin.register(RelevanteZaakRelatie)
 class RelevanteZaakRelatieAdmin(admin.ModelAdmin):
     list_display = ("zaak", "_relevant_zaak", "_relevant_zaak_url")
+    list_filter = ("aard_relatie",)
+    search_fields = ("zaak", "_relevant_zaak", "_relevant_zaak_url")
+    ordering = ("zaak", "_relevant_zaak", "_relevant_zaak_url")
     raw_id_fields = ["zaak", "_relevant_zaak"]
     list_select_related = ["zaak"]
 
@@ -88,7 +119,10 @@ class RelevanteZaakRelatieAdmin(admin.ModelAdmin):
 class ZaakBesluitAdmin(AuditTrailAdminMixin, UUIDAdminMixin, admin.ModelAdmin):
     list_display = ["zaak", "_besluit", "_besluit_url"]
     list_select_related = ["zaak"]
-    raw_id_fields = ["zaak"]
+    list_filter = ()
+    search_fields = ()
+    ordering = ()
+    raw_id_fields = ["zaak", "_besluit"]
     viewset = "openzaak.components.zaken.api.viewsets.ZaakBesluitViewSet"
 
 
@@ -183,6 +217,7 @@ class ZaakAdmin(AuditTrailAdminMixin, UUIDAdminMixin, admin.ModelAdmin):
     )
     date_hierarchy = "registratiedatum"
     list_filter = ("startdatum", "archiefstatus", "vertrouwelijkheidaanduiding")
+    ordering = ("identificatie", "startdatum")
     inlines = [
         StatusInline,
         ZaakObjectInline,
