66925306b73f96ac5fe8400eb7c06d6539b2108a
annashamray <anna.shamray@maykinmedia.nl>
2020-01-23 12:54:40 +0100

    protect nlx config view

--- src/openzaak/config/admin_urls.py
+++ src/openzaak/config/admin_urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from .admin_views import NLXConfigView
 
-app_name = 'config'
+app_name = "config"
 
 urlpatterns = [
-    path(r'nlx', NLXConfigView.as_view(), name='nlx_config'),
+    path(r"nlx", NLXConfigView.as_view(), name="nlx_config"),
 ]
