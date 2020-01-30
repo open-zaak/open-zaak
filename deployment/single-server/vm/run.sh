9c8ac222ad3d2bbccbcbb7611ca138e36fb5f3af
Sergei Maertens <sergei@maykinmedia.nl>
2020-01-30 11:45:56 +0100

    :whale: Include a volume for ON .env file

--- deployment/single-server/vm/run.sh
+++ deployment/single-server/vm/run.sh
@@ -11,6 +11,7 @@ cp $PUBKEY /tmp/openzaak-authorized_keys
 docker build -t openzaak-vm .
 
 docker volume create openzaak-vm
+docker volume create opennotificaties-vm
 docker run \
     --rm \
     --name openzaak-vm \
@@ -18,4 +19,5 @@ docker run \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /tmp/openzaak-authorized_keys:/root/.ssh/authorized_keys \
     -v openzaak-vm:/home/openzaak \
+    -v opennotificaties-vm:/home/opennotificaties \
     openzaak-vm &
