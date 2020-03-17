15bf09d409aa2fc25a0246280330f1db2e97177b
annashamray <anna.shamray@maykinmedia.nl>
2020-03-17 14:39:19 +0100

    :heavy_plus_sign: add django-formtools

--- src/openzaak/conf/includes/base.py
+++ src/openzaak/conf/includes/base.py
@@ -108,6 +108,7 @@ INSTALLED_APPS = [
     "django_filters",
     "django_db_logger",
     "corsheaders",
+    "formtools",
     "vng_api_common",  # before drf_yasg to override the management command
     "vng_api_common.authorizations",
     "vng_api_common.audittrails",
