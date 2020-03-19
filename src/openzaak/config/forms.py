0c938efacf56446562a68d850b99026b68e3d42c
annashamray <anna.shamray@maykinmedia.nl>
2020-03-19 15:53:02 +0100

    validate outway address

--- src/openzaak/config/forms.py
+++ src/openzaak/config/forms.py
@@ -1,4 +1,7 @@
-from django.forms import ModelForm
+import socket
+from urllib.parse import urlparse
+
+from django.forms import ModelForm, ValidationError
 
 from .models import NLXConfig
 
@@ -7,3 +10,21 @@ class NLXConfigForm(ModelForm):
     class Meta:
         model = NLXConfig
         fields = ("directory", "outway")
+
+    def clean_outway(self):
+        outway = self.cleaned_data["outway"]
+
+        if not outway:
+            return outway
+
+        # try to tcp connect to the port
+        parsed = urlparse(outway)
+        with socket.socket() as s:
+            try:
+                s.connect((parsed.hostname, parsed.port))
+            except ConnectionRefusedError:
+                raise ValidationError(
+                    "Connection refused. Please, provide a correct address"
+                )
+
+        return outway
