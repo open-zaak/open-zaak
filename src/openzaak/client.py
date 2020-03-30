3b93bcc0ec0e622b971147cdbb7906bb796f94df
annashamray <anna.shamray@maykinmedia.nl>
2020-03-30 18:10:34 +0200

    mock schemas for external oio and zaakbesluit cases

--- src/openzaak/client.py
+++ src/openzaak/client.py
@@ -1,6 +1,7 @@
 """
 Provide utilities to interact with other APIs as a client.
 """
+from zgw_consumers.client import UnknownService
 from zgw_consumers.models import Service
 
 
@@ -9,5 +10,7 @@ def fetch_object(resource: str, url: str) -> dict:
     Fetch a remote object by URL.
     """
     client = Service.get_client(url)
+    if not client:
+        raise UnknownService(f"{url} API should be added to Service model")
     obj = client.retrieve(resource, url=url)
     return obj
