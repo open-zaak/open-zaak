eb7044f143ba9c2274c0d45708cacc20765f2231
Steven Bal <steven@maykinmedia.nl>
2020-04-10 10:06:07 +0200

    Added test for EIO admin create page

---
+++ src/openzaak/components/documenten/tests/admin/test_eio.py
@@ -0,0 +1,49 @@
+from django.urls import reverse
+
+from django_webtest import WebTest
+from webtest import Upload
+
+from openzaak.accounts.tests.factories import SuperUserFactory
+from openzaak.components.catalogi.tests.factories import InformatieObjectTypeFactory
+
+from ...models import EnkelvoudigInformatieObject
+from ..factories import EnkelvoudigInformatieObjectCanonicalFactory
+
+
+class EnkelvoudigInformatieObjectAdminTests(WebTest):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = SuperUserFactory.create()
+
+    def setUp(self):
+        super().setUp()
+
+        self.app.set_user(self.user)
+
+    def test_add_informatieobject_page(self):
+        add_url = reverse("admin:documenten_enkelvoudiginformatieobject_add")
+
+        response = self.app.get(add_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_informatieobject_save(self):
+        informatieobjecttype = InformatieObjectTypeFactory.create(concept=False)
+        canonical = EnkelvoudigInformatieObjectCanonicalFactory.create(
+            latest_version=None
+        )
+        add_url = reverse("admin:documenten_enkelvoudiginformatieobject_add")
+
+        response = self.app.get(add_url)
+        form = response.form
+
+        form["canonical"] = canonical.pk
+        form["bronorganisatie"] = "000000000"
+        form["creatiedatum"] = "2010-01-01"
+        form["_informatieobjecttype"] = informatieobjecttype.pk
+        form["titel"] = "test"
+        form["auteur"] = "test"
+        form["taal"] = "nld"
+        form["inhoud"] = Upload("stuff.txt", b"")
+
+        response = form.submit(name="_continue")
+        self.assertEqual(response.status_code, 200)
