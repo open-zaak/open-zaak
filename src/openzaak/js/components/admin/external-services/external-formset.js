eacd48914b1b2c6a9c5006f1ad4da143d550b5fa
annashamray <anna.shamray@maykinmedia.nl>
2020-04-10 16:23:46 +0200

    process PR feedback

--- src/openzaak/js/components/admin/external-services/external-formset.js
+++ src/openzaak/js/components/admin/external-services/external-formset.js
@@ -52,11 +52,11 @@ function ExternalFormSet(props) {
                     <thead>
                         <tr>
                             <th className='external-form__hidden'></th>
-                            <th className='external-formset__col'>{% trans "Service" %}</th>
+                            <th className='external-formset__col'>Service</th>
                             <th className='external-formset__col'>API type</th>
-                            <th className='external-formset__col--big'>{% trans "URL" %}</th>
-                            <th className='external-formset__col'>{% trans "NLX outway URL" %}</th>
-                            <th className='external-formset__col--big'>{% trans "OAS URL" %}</th>
+                            <th className='external-formset__col--big'>URL</th>
+                            <th className='external-formset__col'>NLX outway URL</th>
+                            <th className='external-formset__col--big'>OAS URL</th>
                             <th className='external-formset__col'>Authorization</th>
                             <th className='external-formset__col--small'>Verwijderen</th>
                         </tr>
