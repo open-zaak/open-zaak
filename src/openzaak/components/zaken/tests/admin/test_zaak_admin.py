61be1ab83c99a95e4d289ad640d9bdb79e8a7719
Steven Bal <steven@maykinmedia.nl>
2020-04-20 15:24:06 +0200

    Added tests for ZaakAdmin with unavailable external resources

---
+++ src/openzaak/components/zaken/tests/admin/test_zaak_admin.py
@@ -0,0 +1,54 @@
+from django.urls import reverse
+
+import requests_mock
+from django_webtest import WebTest
+
+from openzaak.accounts.tests.factories import SuperUserFactory
+from openzaak.components.zaken.tests.factories import (
+    ZaakFactory,
+    ZaakInformatieObjectFactory,
+)
+
+from ...models import ZaakBesluit
+
+
+class ZaaktypeAdminTests(WebTest):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = SuperUserFactory.create()
+
+    def setUp(self):
+        super().setUp()
+
+        self.app.set_user(self.user)
+
+    def test_zaaktype_detail_external_io_not_available(self):
+        zaak = ZaakFactory.create()
+        zio = ZaakInformatieObjectFactory.create(zaak=zaak)
+        zio._informatieobject = None
+        zio._informatieobject_url = "http://bla.com/404"
+        zio.save()
+
+        url = reverse("admin:zaken_zaak_change", args=(zaak.pk,))
+
+        with requests_mock.Mocker() as m:
+            m.get("http://bla.com/404", status_code=404, json={})
+            response = self.app.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertIn("http://bla.com/404", response.text)
+
+    def test_zaaktype_detail_external_besluit_not_available(self):
+        zaak = ZaakFactory.create()
+        ZaakBesluit.objects.create(zaak=zaak, _besluit_url="http://bla.com/404")
+
+        url = reverse("admin:zaken_zaak_change", args=(zaak.pk,))
+
+        with requests_mock.Mocker() as m:
+            m.get("http://bla.com/404", status_code=404, json={})
+            response = self.app.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertIn("http://bla.com/404", response.text)
