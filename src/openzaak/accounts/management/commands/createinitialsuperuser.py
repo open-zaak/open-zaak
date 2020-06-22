cde70c477db49c34da5d853a99eed3cb666655b8
Steven Bal <steven@maykinmedia.nl>
2020-06-22 17:37:08 +0200

    Added createinitialsuperuser command + tests

---
+++ src/openzaak/accounts/management/commands/createinitialsuperuser.py
@@ -0,0 +1,48 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from django.core.management.base import BaseCommand
+from django.urls import exceptions, reverse
+
+
+class Command(BaseCommand):
+    help = "Creates an initial superuser account and mails the credentials to the specified email"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "username", help="Specifies the username for the superuser.",
+        )
+        parser.add_argument(
+            "email", help="Specifies the email for the superuser.",
+        )
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        username = options["username"]
+        email = options["email"]
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write(
+                self.style.WARNING("Initial superuser already exists, doing nothing")
+            )
+            return
+
+        password = User.objects.make_random_password(length=20)
+        User.objects.create_superuser(username=username, email=email, password=password)
+
+        try:
+            link = f'{settings.ALLOWED_HOSTS[0]}{reverse("admin:index")}'
+        except IndexError:
+            link = "unknown url"
+        except exceptions.NoReverseMatch:
+            link = settings.ALLOWED_HOSTS[0]
+
+        send_mail(
+            f"Credentials for {settings.PROJECT_NAME} ({link})",
+            f"Credentials for project: {settings.PROJECT_NAME}\n\nUsername: {username}\nPassword: {password}",
+            settings.DEFAULT_FROM_EMAIL,
+            [email],
+            fail_silently=False,
+        )
+
+        self.stdout.write(self.style.SUCCESS("Initial superuser successfully created"))
---
+++ src/openzaak/accounts/tests/test_createinitialsuperuser.py
@@ -0,0 +1,49 @@
+from django.conf import settings
+from django.core import mail
+from django.core.management import call_command
+from django.test import TestCase, override_settings
+from django.urls import exceptions, reverse
+
+from ..models import User
+
+
+class CreateInitialSuperuserTests(TestCase):
+    def test_create_initial_superuser_command(self):
+        call_command("createinitialsuperuser", "maykin", "support@maykinmedia.nl")
+        user = User.objects.get()
+
+        self.assertTrue(user.has_usable_password())
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+
+        self.assertEqual(len(mail.outbox), 1)
+
+        sent_mail = mail.outbox[0]
+        try:
+            link = f'{settings.ALLOWED_HOSTS[0]}{reverse("admin:index")}'
+        except exceptions.NoReverseMatch:
+            link = settings.ALLOWED_HOSTS[0]
+        self.assertEqual(
+            sent_mail.subject, f"Credentials for {settings.PROJECT_NAME} ({link})"
+        )
+        self.assertListEqual(sent_mail.recipients(), ["support@maykinmedia.nl"])
+
+    @override_settings(ALLOWED_HOSTS=[])
+    def test_create_initial_superuser_command_allowed_hosts_empty(self):
+        call_command("createinitialsuperuser", "maykin", "support@maykinmedia.nl")
+        user = User.objects.get()
+
+        self.assertTrue(user.has_usable_password())
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+
+        self.assertEqual(len(mail.outbox), 1)
+
+        sent_mail = mail.outbox[0]
+        link = "unknown url"
+        self.assertEqual(
+            sent_mail.subject, f"Credentials for {settings.PROJECT_NAME} ({link})"
+        )
+        self.assertListEqual(sent_mail.recipients(), ["support@maykinmedia.nl"])
