2b26f672feda48b378c37b7bdaf90fd8dc6f5fd8
Sergei Maertens <sergei@maykinmedia.nl>
2020-06-25 10:08:18 +0200

    :green_heart: Fix tests with --keepdb

---
+++ src/openzaak/config/tests/factories.py
@@ -0,0 +1,11 @@
+import factory
+import factory.fuzzy
+from vng_api_common.constants import ComponentTypes
+
+
+class InternalServiceFactory(factory.django.DjangoModelFactory):
+    api_type = factory.fuzzy.FuzzyChoice(ComponentTypes.values)
+
+    class Meta:
+        model = "config.InternalService"
+        django_get_or_create = ("api_type",)
