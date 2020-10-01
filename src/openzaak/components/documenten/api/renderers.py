fe6fcee5e6d29932b79a70b823347efa541efe41
silvia <silvia@maykinmedia.nl>
2020-10-01 10:43:45 +0200

    [#584] BinaryFileRenderer checks for string data

--- src/openzaak/components/documenten/api/renderers.py
+++ src/openzaak/components/documenten/api/renderers.py
@@ -10,4 +10,9 @@ class BinaryFileRenderer(BaseRenderer):
     render_style = "binary"
 
     def render(self, data, media_type=None, renderer_context=None):
+        # When trying to download a non-existing file, `data` contains a string instead of binary data.
+        # The string needs to be encoded as UTF-8, or it will be coerced into a bytestring using self.charset,
+        # which will cause an error (issue #584)
+        if isinstance(data, str):
+            return data.encode("utf-8")
         return data
