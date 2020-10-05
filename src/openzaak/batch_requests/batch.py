270e9e557482c9c3a5408010af7846155d853b88
Sergei Maertens <sergei@maykinmedia.nl>
2020-10-05 16:41:43 +0200

    :alembic: Batch requests

--- src/openzaak/batch_requests/batch.py
+++ src/openzaak/batch_requests/batch.py
@@ -1,4 +1,5 @@
 from concurrent.futures import ThreadPoolExecutor as parallel
+from copy import deepcopy
 from dataclasses import dataclass
 from typing import List
 
@@ -13,6 +14,15 @@ class BatchRequest:
 
 
 def batch_requests(parent: Request, requests: List[BatchRequest]) -> List[Response]:
+    responses = []
+    for request in requests:
+        parent_copy = deepcopy(parent)
+        response = handle_batch_request(parent_copy, request)
+        responses.append(response)
+    return responses
+
+
+def handle_batch_request(parent: Request, request: BatchRequest) -> Response:
     import bpdb
 
     bpdb.set_trace()
