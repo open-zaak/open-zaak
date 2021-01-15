1f3e3c87d2942c39cca117443095fea32dbcaeb3
silvia <silvia@maykinmedia.nl>
2021-01-15 11:32:02 +0100

    :green_heart: [#798] Heisen bug

--- src/openzaak/components/documenten/tests/test_audittrails_cmis.py
+++ src/openzaak/components/documenten/tests/test_audittrails_cmis.py
@@ -187,7 +187,9 @@ class AuditTrailTests(JWTAuthMixin, APICMISTestCase):
             informatieobject_url, {"titel": "changed", "lock": eio_canonical.lock},
         ).data
 
-        audittrails = AuditTrail.objects.filter(hoofd_object=informatieobject_url)
+        audittrails = AuditTrail.objects.filter(
+            hoofd_object=informatieobject_url
+        ).order_by("pk")
         self.assertEqual(audittrails.count(), 2)
 
         # Verify that the audittrail for the EnkelvoudigInformatieObject
