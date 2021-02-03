94e37cb0a8f8bfdd5afcd4b3e4844ded8ec4ed77
Steven Bal <steven@maykinmedia.nl>
2021-02-03 12:09:20 +0100

    :green_heart: Temporarily ignore broken commonground link in docs

--- docs/conf.py
+++ docs/conf.py
@@ -82,6 +82,8 @@ linkcheck_ignore = [
     r"https?://.*\.gemeente.nl",
     r"http://localhost:\d+/",
     r"https://.*sentry\.openzaak\.nl.*",
+    # TODO temporary workaround for issue #6218 with Pleio
+    r"https://commonground.nl/groups/view/d9c2f667-2f3e-4153-a79b-57dde7f56cc2/team-open-zaak",
 ]
 
 sphinx_tabs_valid_builders = ["linkcheck"]
