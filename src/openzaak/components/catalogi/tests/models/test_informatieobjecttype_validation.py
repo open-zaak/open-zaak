a662d35235c2324623ebca5415460a409b0c141e
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2021-02-24 15:14:51 +0100

    Fixes #863 -- Add unit test

---
+++ src/openzaak/components/catalogi/tests/models/test_informatieobjecttype_validation.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+
+from django.core.exceptions import ValidationError
+
+from ..factories import CatalogusFactory, InformatieObjectTypeFactory
+
+
+class InformatieobjecttypeValidationTests(TestCase):
+    """
+    Test the validation on Informatieobjecttype
+    """
+
+    def test_assertion_raised_when_dates_overlap(self):
+        catalogus = CatalogusFactory.create()
+
+        InformatieObjectTypeFactory.create(
+            catalogus=catalogus,
+            omschrijving="test",
+            datum_begin_geldigheid="2018-01-01",
+            datum_einde_geldigheid="2018-12-31",
+        )
+
+        instance = InformatieObjectTypeFactory.create(
+            catalogus=catalogus,
+            omschrijving="test",
+            datum_begin_geldigheid="2018-10-10",
+        )
+
+        with self.assertRaises(ValidationError):
+            instance.clean()
