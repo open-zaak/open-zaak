4e75fa4b40c95a3a3fc0beef16647c24b0119294
Alex de Landgraaf <alex@maykinmedia.nl>
2021-03-16 14:15:00 +0100

    Removing disabled newrelic APM monitoring

--- src/openzaak/wsgi.py
+++ src/openzaak/wsgi.py
@@ -14,23 +14,5 @@ from django.core.wsgi import get_wsgi_application
 
 from openzaak.setup import setup_env
 
-
-def init_newrelic():
-    if os.environ.get("PROJECT_ROOT"):
-        try:
-            import newrelic.agent
-
-            newrelic.agent.initialize(
-                os.path.join(os.environ.get("PROJECT_ROOT"), "newrelic.ini"),
-                "production",
-            )
-        except Exception as e:
-            print("Could not initialize New Relic APM, ignoring:")
-            print(e)
-
-
-# Enable New Relic on production
-# init_newrelic()
-
 setup_env()
 application = get_wsgi_application()
