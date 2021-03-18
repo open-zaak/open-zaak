5483ffc0435f58d3cba94d9299c58b0ecdd2554d
Sergei Maertens <sergei@maykinmedia.nl>
2021-03-18 18:09:14 +0100

    :alien: Update to pip-tools 6

--- bin/compile_dependencies.cmd
+++ bin/compile_dependencies.cmd
@@ -6,19 +6,16 @@ cd %toplevel%
 
 REM Base deps
 pip-compile^
-    --no-index^
     requirements/base.in
 
 REM Dependencies for testing
 pip-compile^
-    --no-index^
     --output-file requirements/ci.txt^
     requirements/base.txt^
     requirements/test-tools.in
 
 REM Dev depedencies - exact same set as CI + some extra tooling
 pip-compile^
-    --no-index^
     --output-file requirements/dev.txt^
     requirements/ci.txt^
     requirements/dev.in
--- bin/compile_dependencies.sh
+++ bin/compile_dependencies.sh
@@ -8,19 +8,16 @@ cd $toplevel
 
 # Base (& prod) deps
 pip-compile \
-    --no-index \
     requirements/base.in
 
 # Dependencies for testing
 pip-compile \
-    --no-index \
     --output-file requirements/ci.txt \
     requirements/base.txt \
     requirements/test-tools.in
 
 # Dev depedencies - exact same set as CI + some extra tooling
 pip-compile \
-    --no-index \
     --output-file requirements/dev.txt \
     requirements/ci.txt \
     requirements/dev.in
