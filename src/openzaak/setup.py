f9ffc5b1caa82a53ebb4898f4a6995a42d57fca2
Sergei Maertens <sergei@maykinmedia.nl>
2021-03-24 10:47:46 +0100

    :alien: Revert camelize behaviour back to 0.2.0 version

--- src/openzaak/setup.py
+++ src/openzaak/setup.py
@@ -12,6 +12,7 @@ they are available for Django settings initialization.
     before Django is initialized.
 """
 import os
+import re
 import tempfile
 
 from dotenv import load_dotenv
@@ -29,8 +30,29 @@ def setup_env():
 
     load_self_signed_certs()
 
+    monkeypatch_camelize_re()
+
 
 def load_self_signed_certs() -> None:
     # create target directory for resulting combined certificate file
     target_dir = tempfile.mkdtemp()
     _load_self_signed_certs(target_dir)
+
+
+def monkeypatch_camelize_re() -> None:
+    """
+    Revert the camelize_re back to the old behaviour.
+
+    drf-camel-case had a camelize_re that excluded numbers, which was used while the
+    standard was created. When upgrading drf-camel-case, we had to revert this regex
+    back to the old one to stay compliant with the schema from the standard.
+
+    TODO: bring up this more correct camelizing issue for a 2.x version of the standard
+    where breaking changes are allowed.
+
+    One of the relevant commits:
+    https://github.com/vbabiy/djangorestframework-camel-case/commit/f814bf32461d274e99bf4f24dcd6bac06056c8b2#
+    """
+    from djangorestframework_camel_case import util
+
+    util.camelize_re = re.compile(r"[a-z]?_[a-z]")
