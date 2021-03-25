9d41b634df0e612c9f719f3e766052105654cb77
Sergei Maertens <sergei@maykinmedia.nl>
2021-03-25 21:49:23 +0100

    :sparkles: Fixes #916 -- enable opt-in to use X-Forwarded-Host headers
    Caveats:
    
    * Django only uses this header if the setting is explicitly enabled
    * Only a single value is supported. This header can occur as
      comma-separated list if multiple upstream proxies are used. It's up to
      your infrastructure to ensure only a single value is passed to Django.
    
    Use this if you cannot set the `Host` header (for example, using istio
    in Kubernetes).

---
+++ src/openzaak/tests/test_x_forwarded_host.py
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: EUPL-1.2
+# Copyright (C) 2021 Dimpact
+from django.test import RequestFactory, SimpleTestCase, override_settings, tag
+
+
+@tag("x-forwarded-for")
+class XForwardedHostTests(SimpleTestCase):
+    """
+    Test the opt-in behaviour of the X-Forwarded-For header.
+    """
+
+    factory = RequestFactory()
+
+    def test_default_disabled(self):
+        request = self.factory.get("/", HTTP_X_FORWARDED_HOST="evil.com",)
+
+        self.assertEqual(
+            request.get_host(), "testserver",
+        )
+
+    @override_settings(
+        USE_X_FORWARDED_HOST=True, ALLOWED_HOSTS=["upstream.proxy"],
+    )
+    def test_enabled(self):
+        request = self.factory.get("/", HTTP_X_FORWARDED_HOST="upstream.proxy",)
+
+        self.assertEqual(
+            request.get_host(), "upstream.proxy",
+        )
