4d4e2b7a2367ef7f77aecb2db5b94844cf4f3825
Sergei Maertens <sergei@maykinmedia.nl>
2021-04-07 13:05:05 +0200

    :art: Improve compile_dependencies script
    It now forwards any arguments to the underlying pip-compile calls.

---
+++ bin/compile_dependencies.bat
@@ -0,0 +1,30 @@
+@echo off
+
+for /F "tokens=1" %%i in ('git rev-parse --show-toplevel') do set toplevel=%%i
+
+cd %toplevel%
+
+REM we are echo'ing the Linux variant here, simply because we have more Linux users
+set CUSTOM_COMPILE_COMMAND="./bin/compile_dependencies.sh"
+
+REM Base deps
+pip-compile^
+    --no-emit-index-url^
+    %*^
+    requirements/base.in
+
+REM Dependencies for testing
+pip-compile^
+    --no-emit-index-url^
+    --output-file requirements/ci.txt^
+    %*^
+    requirements/base.txt^
+    requirements/test-tools.in
+
+REM Dev depedencies - exact same set as CI + some extra tooling
+pip-compile^
+    --no-emit-index-url^
+    --output-file requirements/dev.txt^
+    %*^
+    requirements/ci.txt^
+    requirements/dev.in
