22443d5a1fe1b94b6c4b3fb125c43a736ea134bf
silvia <silvia@maykinmedia.nl>
2021-04-23 12:58:53 +0200

    :white_check_mark: [#956] Updated tests

--- src/openzaak/components/documenten/tests/models/test_human_readable_identification_cmis.py
+++ src/openzaak/components/documenten/tests/models/test_human_readable_identification_cmis.py
@@ -2,16 +2,16 @@
 # Copyright (C) 2019 - 2020 Dimpact
 from datetime import date
 
-from django.test import TestCase, override_settings, tag
+from django.test import override_settings, tag
 
-from openzaak.utils.tests import CMISMixin
+from openzaak.utils.tests import APICMISTestCase
 
 from ..factories import EnkelvoudigInformatieObjectFactory
 
 
 @tag("cmis")
 @override_settings(CMIS_ENABLED=True)
-class CMISEIOTests(CMISMixin, TestCase):
+class CMISEIOTests(APICMISTestCase):
     def test_default_identificatie_is_equal_to_uuid(self):
         eio = EnkelvoudigInformatieObjectFactory.create(
             identificatie="", creatiedatum=date(2019, 7, 1)
