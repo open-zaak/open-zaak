fda8428c10c39b098ad149ff236012aa8acd457b
Sergei Maertens <sergei@maykinmedia.nl>
2021-06-28 15:48:00 +0200

    :white_check_mark: Refs. #809 -- added tests for admin validation of identificatie fields

---
+++ src/openzaak/components/besluiten/tests/admin/test_besluit_admin.py
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: EUPL-1.2
+# Copyright (C) 2020 Dimpact
+from django.urls import reverse
+
+from django_webtest import WebTest
+
+from openzaak.accounts.tests.factories import SuperUserFactory
+
+from ..factories import BesluitFactory
+
+
+class BesluitAdminTests(WebTest):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = SuperUserFactory.create()
+
+    def setUp(self):
+        super().setUp()
+
+        self.app.set_user(self.user)
+
+    def test_non_alphanumeric_identificatie_validation(self):
+        """
+        Edit a zaak with an identificatie allowed by the API.
+
+        This should not trigger validation errors.
+        """
+        besluit = BesluitFactory.create(identificatie="ZK bläh")
+        url = reverse("admin:besluiten_besluit_change", args=(besluit.pk,))
+        response = self.app.get(url)
+        self.assertEqual(response.form["identificatie"].value, "ZK bläh")
+
+        submit_response = response.form.submit()
+
+        self.assertEqual(submit_response.status_code, 302)
