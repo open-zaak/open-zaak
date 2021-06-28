c7f68bd539f9379b3026a03ea7003232a4a0a85a
Sergei Maertens <sergei@maykinmedia.nl>
2021-06-28 17:13:32 +0200

    :fire: Fixes #949 -- Removed NLX-inway configuration integration
    NLX dropped support for inway-config.toml files, so there's no point
    anymore in us generating this configuration. Use NLX management instead.

--- src/openzaak/config/admin_urls.py
+++ src/openzaak/config/admin_urls.py
@@ -7,13 +7,11 @@ from .admin_views import (
     ExternalConfigView,
     InternalConfigView,
     NLXConfigView,
-    NLXInwayView,
 )
 
 app_name = "config"
 
 urlpatterns = [
-    path(r"nlx-inway", NLXInwayView.as_view(), name="nlx-inway"),
     path(r"detail", ConfigDetailView.as_view(), name="config-detail"),
     path(r"nlx", NLXConfigView.as_view(), name="config-nlx"),
     path(r"internal", InternalConfigView.as_view(), name="config-internal"),
