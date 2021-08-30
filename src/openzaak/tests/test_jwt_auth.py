f9360eb8ecf1d66e06895cf41a74cf38f563fd2b
Sergei Maertens <sergei@maykinmedia.nl>
2021-08-30 22:31:16 +0200

    :white_check_mark: Refs. #936 -- added crashing regression test for user_id: null claim

--- src/openzaak/tests/test_jwt_auth.py
+++ src/openzaak/tests/test_jwt_auth.py
@@ -8,7 +8,9 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from vng_api_common.tests import reverse
 
+from openzaak.components.autorisaties.middleware import JWTAuth
 from openzaak.components.zaken.tests.factories import ZaakFactory
+from openzaak.components.zaken.tests.utils import ZAAK_WRITE_KWARGS
 from openzaak.utils.tests import JWTAuthMixin, generate_jwt_auth
 
 
@@ -94,3 +96,30 @@ class JWTLeewayTests(JWTAuthMixin, APITestCase):
         response = self.client.get(zaak_url)
 
         self.assertNotEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class JWTRegressionTests(JWTAuthMixin, APITestCase):
+    heeft_alle_autorisaties = True
+
+    def test_null_user_id(self):
+        """
+        Assert that ``user_id: null`` claim does not crash Open Zaak.
+
+        Regression test for https://github.com/open-zaak/open-zaak/issues/936
+        """
+        token = generate_jwt_auth(
+            self.client_id,
+            self.secret,
+            user_id=None,
+            user_representation=self.user_representation,
+            nbf=int(timezone.now().timestamp()),
+        )
+        self.client.credentials(HTTP_AUTHORIZATION=token)
+        zaak = ZaakFactory.create()
+        endpoint = reverse(zaak)
+        payload = JWTAuth(token.split(" ")[1]).payload
+        assert payload["user_id"] is None
+
+        response = self.client.patch(endpoint, data={}, **ZAAK_WRITE_KWARGS)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
