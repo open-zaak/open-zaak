f52f12180bc1ed6af31e768a2e529fd4089c0b40
Sergei Maertens <sergei@maykinmedia.nl>
2021-10-28 08:57:30 +0200

    :zap: Enable DB connection re-use
    Configurable via the environment variable DB_CONN_MAX_AGE

--- src/openzaak/conf/docker.py
+++ src/openzaak/conf/docker.py
@@ -6,6 +6,7 @@ os.environ.setdefault("DB_HOST", "db")
 os.environ.setdefault("DB_NAME", "postgres")
 os.environ.setdefault("DB_USER", "postgres")
 os.environ.setdefault("DB_PASSWORD", "")
+os.environ.setdefault("DB_CONN_MAX_AGE", "60")
 
 os.environ.setdefault("LOG_STDOUT", "yes")
 
