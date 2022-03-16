228458d8641eb1d778ae6f39a52cc3cb07ba7632
Sergei Maertens <sergei@maykinmedia.nl>
2022-03-16 15:20:51 +0100

    :alien: [#1098] -- handle the relativedeltafield changes
    * Fixed the admin widget override so our own split-version is still used

--- src/openzaak/forms/fields.py
+++ src/openzaak/forms/fields.py
@@ -1,12 +1,13 @@
 # SPDX-License-Identifier: EUPL-1.2
 # Copyright (C) 2019 - 2020 Dimpact
-from django import forms
 from django.core import validators
+
+from relativedeltafield.forms import RelativeDeltaFormField
 
 from .widgets import SplitRelativeDeltaWidget
 
 
-class RelativeDeltaField(forms.CharField):
+class RelativeDeltaField(RelativeDeltaFormField):
     widget = SplitRelativeDeltaWidget
     empty_strings_allowed = False
     empty_values = list(validators.EMPTY_VALUES) + ["P0D"]
