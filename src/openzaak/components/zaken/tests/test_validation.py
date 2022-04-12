3e9a4d4fbcfc051e20ed8fa21a76d2db0dc32096
Sergei Maertens <sergei@maykinmedia.nl>
2022-04-12 11:26:49 +0200

    :clown_face: Mock out referentielijst API spec retrieval properly

--- src/openzaak/components/zaken/tests/test_validation.py
+++ src/openzaak/components/zaken/tests/test_validation.py
@@ -34,6 +34,7 @@ from openzaak.components.catalogi.tests.factories import (
 from openzaak.components.documenten.tests.factories import (
     EnkelvoudigInformatieObjectFactory,
 )
+from openzaak.tests.utils import mock_service_oas_get
 from openzaak.utils.tests import JWTAuthMixin, get_eio_response, mock_client
 
 from ..constants import AardZaakRelatie, BetalingsIndicatie
@@ -177,12 +178,9 @@ class ZaakValidationTests(JWTAuthMixin, APITestCase):
         body = {"communicatiekanaal": communicatiekanaal_url}
 
         with requests_mock.Mocker() as m:
+            mock_service_oas_get(m, "vrl", oas_url=settings.REFERENTIELIJSTEN_API_SPEC)
             m.get(communicatiekanaal_url, status_code=200, json={"something": "wrong"})
-            m.get(
-                settings.REFERENTIELIJSTEN_API_SPEC,
-                json={},
-                headers={"X-OAS-Version": "3.0"},
-            )
+
             response = self.client.post(url, body, **ZAAK_WRITE_KWARGS)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
