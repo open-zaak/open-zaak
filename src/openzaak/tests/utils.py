ddd8a79d5a59ebd4816abe3c8ba8852546d263f2
Sergei Maertens <sergei@maykinmedia.nl>
2022-04-12 11:12:06 +0200

    :art: Remove OAS mocking duplication

--- src/openzaak/tests/utils.py
+++ src/openzaak/tests/utils.py
@@ -8,21 +8,12 @@ MOCK_FILES_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "schem
 
 NRC_API_SPEC = os.path.join(MOCK_FILES_DIR, "nrc.yaml")
 
-CONTACTMOMENTEN_API_SPEC = os.path.join(MOCK_FILES_DIR, "contactmomenten.yaml")
-
 _CACHE = {}
 
 
 def mock_nrc_oas_get(m: Mocker) -> None:
     oas_url = "https://notificaties-api.vng.cloud/api/v1/schema/openapi.yaml?v=3"
-    with open(NRC_API_SPEC, "rb") as api_spec:
-        m.get(oas_url, content=api_spec.read())
-
-
-def mock_contactmomenten_oas_get(m: Mocker, url: str) -> None:
-    oas_url = f"{url}schema/openapi.yaml?v=3"
-    with open(CONTACTMOMENTEN_API_SPEC, "rb") as api_spec:
-        m.get(oas_url, content=api_spec.read())
+    mock_service_oas_get(m, "nrc", oas_url=oas_url)
 
 
 def mock_service_oas_get(
