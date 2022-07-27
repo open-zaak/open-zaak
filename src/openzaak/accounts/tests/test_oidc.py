65b58ae5dc685e5d086607c94788787e927d12b1
Steven Bal <steven@maykinmedia.nl>
2022-07-27 16:14:27 +0200

    :white_check_mark: [#1233] Add test for OIDC sessionrefresh

--- src/openzaak/accounts/tests/test_oidc.py
+++ src/openzaak/accounts/tests/test_oidc.py
@@ -1,10 +1,15 @@
 # SPDX-License-Identifier: EUPL-1.2
 # Copyright (C) 2021 Dimpact
+from unittest.mock import patch
+from urllib.parse import urlparse
+
 from django.urls import reverse
 from django.utils.translation import gettext as _
 
 from django_webtest import WebTest
 from mozilla_django_oidc_db.models import OpenIDConnectConfig
+
+from .factories import SuperUserFactory
 
 
 class OIDCLoginButtonTestCase(WebTest):
@@ -42,3 +47,31 @@ class OIDCLoginButtonTestCase(WebTest):
         self.assertEqual(
             oidc_login_link.attrs["href"], reverse("oidc_authentication_init")
         )
+
+
+class AdminSessionRefreshMiddlewareTests(WebTest):
+    @patch(
+        "mozilla_django_oidc_db.mixins.OpenIDConnectConfig.get_solo",
+        return_value=OpenIDConnectConfig(
+            enabled=True, oidc_op_authorization_endpoint="https://example.com/auth/",
+        ),
+    )
+    @patch(
+        "mozilla_django_oidc.middleware.SessionRefresh.is_refreshable_url",
+        return_value=True,
+    )
+    def test_session_refresh_no_crash(self, *mocks):
+        """
+        Regression test for crash on admin login because of session refresh.
+        """
+        user = SuperUserFactory.create()
+        admin_url = reverse("admin:index")
+
+        response = self.app.get(admin_url, user=user)
+
+        # we are being redirected to OIDC
+        self.assertEqual(response.status_code, 302)
+        redirect_url = urlparse(response["Location"])
+        self.assertEqual(redirect_url.scheme, "https")
+        self.assertEqual(redirect_url.netloc, "example.com")
+        self.assertEqual(redirect_url.path, "/auth/")
