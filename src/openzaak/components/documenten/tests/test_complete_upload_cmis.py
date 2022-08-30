d325286e15d699caa4c52a31414eb71242225e80
Steven Bal <steven@maykinmedia.nl>
2022-08-30 10:09:15 +0200

    :white_check_mark: [#1223] Add complete upload tests for CMIS

---
+++ src/openzaak/components/documenten/tests/test_complete_upload_cmis.py
@@ -0,0 +1,67 @@
+# SPDX-License-Identifier: EUPL-1.2
+# Copyright (C) 2022 Dimpact
+from io import BytesIO
+
+from django.core.files import File
+from django.test import override_settings
+
+from rest_framework.test import APITestCase
+
+from openzaak.components.documenten.models import BestandsDeel
+
+from .factories import BestandsDeelFactory, EnkelvoudigInformatieObjectFactory
+
+
+@override_settings(
+    DOCUMENTEN_UPLOAD_CHUNK_SIZE=10, CMIS_ENABLED=True,
+)
+class UploadTestCase(APITestCase):
+    def test_complete_upload_true(self):
+        eio = EnkelvoudigInformatieObjectFactory.create()
+        BestandsDeelFactory.create(
+            informatieobject=None, informatieobject_uuid=eio.uuid
+        )
+
+        self.assertTrue(
+            BestandsDeel.objects.filter(informatieobject_uuid=eio.uuid).complete_upload
+        )
+
+    def test_complete_upload_false(self):
+        eio = EnkelvoudigInformatieObjectFactory.create()
+        BestandsDeelFactory.create(
+            informatieobject=None, informatieobject_uuid=eio.uuid
+        )
+        BestandsDeelFactory.create(
+            informatieobject=None, informatieobject_uuid=eio.uuid, inhoud=None, omvang=9
+        )
+
+        self.assertFalse(
+            BestandsDeel.objects.filter(informatieobject_uuid=eio.uuid).complete_upload
+        )
+
+    def test_complete_part_true(self):
+        eio = EnkelvoudigInformatieObjectFactory.create()
+        part = BestandsDeelFactory.create(
+            informatieobject=None, informatieobject_uuid=eio.uuid
+        )
+
+        self.assertTrue(part.voltooid)
+
+    def test_complete_part_false_no_file_uploaded(self):
+        eio = EnkelvoudigInformatieObjectFactory.create()
+        part = BestandsDeelFactory.create(
+            informatieobject=None, informatieobject_uuid=eio.uuid, inhoud=None, omvang=9
+        )
+
+        self.assertFalse(part.voltooid)
+
+    def test_complete_part_false_file_size_mismatch(self):
+        eio = EnkelvoudigInformatieObjectFactory.create()
+        part = BestandsDeelFactory.create(
+            informatieobject=None,
+            informatieobject_uuid=eio.uuid,
+            inhoud=File(BytesIO(b"12345678"), name="somefile"),
+            omvang=9,
+        )
+
+        self.assertFalse(part.voltooid)
