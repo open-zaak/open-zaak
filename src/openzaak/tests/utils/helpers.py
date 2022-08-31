6cfca3586ad226d837188d74ec2536d90ae5d287
Sergei Maertens <sergei@maykinmedia.nl>
2022-08-31 16:47:47 +0200

    :recycle: Move can_connect helper to test utilities

---
+++ src/openzaak/tests/utils/helpers.py
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: EUPL-1.2
+# Copyright (C) 2022 Dimpact
+import socket
+
+
+def can_connect(hostname: str):
+    # adapted from https://stackoverflow.com/a/28752285
+    hostname, port = hostname.split(":")
+    try:
+        host = socket.gethostbyname(hostname)
+        s = socket.create_connection((host, int(port)), 2)
+        s.close()
+        return True
+    except Exception:
+        return False
