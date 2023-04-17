f7185b027c9b0a36b29b5067265b57fcb6836168
Anna Shamray <anna.shamray@maykinmedia.nl>
2023-04-17 17:37:40 +0200

    :sparkles: [#1361] make count a cached property

--- src/openzaak/components/zaken/api/pagination.py
+++ src/openzaak/components/zaken/api/pagination.py
@@ -1,11 +1,13 @@
 # SPDX-License-Identifier: EUPL-1.2
 # Copyright (C) 2023 Dimpact
 from django.core.paginator import Paginator as DjangoPaginator
+from django.utils.functional import cached_property
 
 from rest_framework.pagination import PageNumberPagination
 
 
 class ZaakPaginator(DjangoPaginator):
+    @cached_property
     def count(self):
         """
         ⚡ restricts values to PK to remove implicit join from SQL query
