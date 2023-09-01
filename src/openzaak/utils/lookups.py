0c67cfc07bfca060453e1a11a824748f56b520c2
Anna Shamray <anna.shamray@maykinmedia.nl>
2023-09-01 10:04:44 +0200

    :recycle: [#1462] make FkOrServiceUrlField lookups DRYer

--- src/openzaak/utils/lookups.py
+++ src/openzaak/utils/lookups.py
@@ -12,12 +12,7 @@ from zgw_consumers.models.lookups import decompose_value
 from .fields import FkOrServiceUrlField
 
 
-@FkOrServiceUrlField.register_lookup
-class Exact(_Exact):
-    """
-    combine Exact lookups for FkOrUrlField and ServiceUrlField
-    """
-
+class FkOrServiceUrlFieldMixin:
     def get_cols(self) -> tuple:
         """ return tuple of cols for local fk, remote base url and remote relative url"""
         target = self.lhs.target
@@ -28,47 +23,6 @@ class Exact(_Exact):
             target._url_field._base_field.get_col(db_table),
             target._url_field._relative_field.get_col(db_table),
         )
-
-    def get_prep_lookup(self):
-        if not self.rhs_is_direct_value():
-            return super().get_prep_lookup()
-
-        if isinstance(self.rhs, ProxyMixin):
-            self.rhs = self.rhs._loose_fk_data["url"]
-
-        if isinstance(self.rhs, str):
-            # dealing with a remote composite URL - return list
-            fk_lhs, base_url_lhs, relative_url_lhs = self.get_cols()
-
-            base_value, relative_value = decompose_value(self.rhs)
-            base_normalized_value = get_normalized_value(base_value)[0]
-            relative__normalized_value = get_normalized_value(relative_value)[0]
-            return [
-                base_url_lhs.field.get_prep_value(base_normalized_value),
-                relative_url_lhs.field.get_prep_value(relative__normalized_value),
-            ]
-
-        # local URl
-        return get_normalized_value(self.rhs)[0]
-
-    def get_db_prep_lookup(self, value, connection):
-        # composite field
-        if isinstance(value, list) and len(value) == 2:
-            target = self.lhs.target
-
-            sql = "%s"
-            params = [
-                target._url_field._base_field.get_db_prep_value(
-                    value[0], connection, prepared=True
-                ),
-                target._url_field._relative_field.get_db_prep_value(
-                    value[1], connection, prepared=True
-                ),
-            ]
-
-            return sql, params
-
-        return super().get_db_prep_lookup(value, connection)
 
     def split_lhs(
         self, compiler, connection
@@ -92,6 +46,68 @@ class Exact(_Exact):
             relative_lhs_sql,
             relative_lhs_params,
         )
+
+    def get_prep_lookup(self) -> list:
+        if not self.rhs_is_direct_value():
+            return super().get_prep_lookup()
+
+        fk_lhs, base_url_lhs, relative_url_lhs = self.get_cols()
+        rhs_values = (
+            self.rhs if self.get_db_prep_lookup_value_is_iterable else [self.rhs]
+        )
+
+        prepared_values = []
+        for rhs_value in rhs_values:
+            if isinstance(rhs_value, ProxyMixin):
+                rhs_value = rhs_value._loose_fk_data["url"]
+
+            if isinstance(rhs_value, str):
+                # dealing with a remote composite URL - return list
+                base_value, relative_value = decompose_value(rhs_value)
+                base_normalized_value = get_normalized_value(base_value)[0]
+                relative__normalized_value = get_normalized_value(relative_value)[0]
+                prepared_value = [
+                    base_url_lhs.field.get_prep_value(base_normalized_value),
+                    relative_url_lhs.field.get_prep_value(relative__normalized_value),
+                ]
+
+            else:
+                # local urls = return their pk
+                prepared_value = get_normalized_value(rhs_value)[0]
+
+            prepared_values.append(prepared_value)
+
+        return (
+            prepared_values[0]
+            if not self.get_db_prep_lookup_value_is_iterable
+            else prepared_values
+        )
+
+
+@FkOrServiceUrlField.register_lookup
+class Exact(FkOrServiceUrlFieldMixin, _Exact):
+    """
+    combine Exact lookups for FkOrUrlField and ServiceUrlField
+    """
+
+    def get_db_prep_lookup(self, value, connection):
+        # composite field
+        if isinstance(value, list) and len(value) == 2:
+            target = self.lhs.target
+
+            sql = "%s"
+            params = [
+                target._url_field._base_field.get_db_prep_value(
+                    value[0], connection, prepared=True
+                ),
+                target._url_field._relative_field.get_db_prep_value(
+                    value[1], connection, prepared=True
+                ),
+            ]
+
+            return sql, params
+
+        return super().get_db_prep_lookup(value, connection)
 
     def as_sql(self, compiler, connection):
         # process lhs
@@ -124,49 +140,16 @@ class Exact(_Exact):
 
 
 @FkOrServiceUrlField.register_lookup
-class In(_In):
+class In(FkOrServiceUrlFieldMixin, _In):
     """
     Split the IN query into two IN queries, per datatype.
 
     Creates an IN query for the url field values, and an IN query for the FK
     field values, joined together by an OR.
+    This realization will add additional DB query for every external url item in rhs list
     """
 
     lookup_name = "in"
-
-    def get_cols(self) -> tuple:
-        """ return tuple of cols for local fk, remote base url and remote relative url"""
-        target = self.lhs.target
-        db_table = target.model._meta.db_table
-
-        return (
-            target._fk_field.get_col(db_table),
-            target._url_field._base_field.get_col(db_table),
-            target._url_field._relative_field.get_col(db_table),
-        )
-
-    def split_lhs(
-        self, compiler, connection
-    ) -> Tuple[str, tuple, str, tuple, str, tuple]:
-
-        fk_lhs, base_url_lhs, relative_url_lhs = self.get_cols()
-
-        fk_lhs_sql, fk_params = self.process_lhs(compiler, connection, lhs=fk_lhs)
-        base_lhs_sql, base_lhs_params = self.process_lhs(
-            compiler, connection, lhs=base_url_lhs
-        )
-        relative_lhs_sql, relative_lhs_params = self.process_lhs(
-            compiler, connection, lhs=relative_url_lhs
-        )
-
-        return (
-            fk_lhs_sql,
-            fk_params,
-            base_lhs_sql,
-            base_lhs_params,
-            relative_lhs_sql,
-            relative_lhs_params,
-        )
 
     def process_rhs(self, compiler, connection):
         """
@@ -248,39 +231,7 @@ class In(_In):
         if not url_sql:
             return fk_sql, fk_params
 
-        params = url_params + fk_params
+        params = url_params + list(fk_params)
         sql = "(%s OR %s)" % (url_sql, fk_sql)
 
         return sql, params
-
-    def get_prep_lookup(self) -> list:
-        if not self.rhs_is_direct_value():
-            return super().get_prep_lookup()
-
-        prepared_values = []
-        for rhs_value in self.rhs:
-            if isinstance(rhs_value, ProxyMixin):
-                rhs_value = rhs_value._loose_fk_data["url"]
-
-            if isinstance(rhs_value, str):
-                # dealing with a remote composite URL - return list
-                fk_lhs, base_url_lhs, relative_url_lhs = self.get_cols()
-
-                base_value, relative_value = decompose_value(rhs_value)
-                base_normalized_value = get_normalized_value(base_value)[0]
-                relative__normalized_value = get_normalized_value(relative_value)[0]
-                prepared_value = [
-                    base_url_lhs.field.get_prep_value(base_normalized_value),
-                    relative_url_lhs.field.get_prep_value(relative__normalized_value),
-                ]
-
-            else:
-                # local urls = return their pk
-                prepared_value = get_normalized_value(rhs_value)[0]
-
-            prepared_values.append(prepared_value)
-
-        return prepared_values
-
-    def get_db_prep_lookup(self, value, connection):
-        return super().get_db_prep_lookup(value, connection)
