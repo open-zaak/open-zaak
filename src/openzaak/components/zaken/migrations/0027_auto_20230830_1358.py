3d795a18c10a7007434b1ce0126711de3545e24f
Anna Shamray <anna.shamray@maykinmedia.nl>
2023-09-18 15:24:20 +0200

    :twisted_rightwards_arrows: [#1455] fix migrations after rebase

--- src/openzaak/components/zaken/migrations/0027_auto_20230830_1358.py
+++ src/openzaak/components/zaken/migrations/0027_auto_20230830_1358.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("zaken", "0025_auto_20230824_1218"),
+        ("zaken", "0026_auto_20230824_1218"),
     ]
 
     operations = [
