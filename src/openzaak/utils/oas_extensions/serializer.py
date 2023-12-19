d6caa6029061325d0d1b8d89185bceef5441d239
Anna Shamray <anna.shamray@maykinmedia.nl>
2023-12-19 15:25:59 +0100

    :sparkles: [#1510] add extension for gegevensgroep

---
+++ src/openzaak/utils/oas_extensions/serializer.py
@@ -0,0 +1,17 @@
+from drf_spectacular.extensions import OpenApiSerializerExtension
+
+
+class GegevensGroepSerializerExtension(OpenApiSerializerExtension):
+    target_class = "vng_api_common.serializers.GegevensGroepSerializer"
+    match_subclasses = True
+
+    def map_serializer(self, auto_schema, direction) -> dict:
+        schema = auto_schema._map_serializer(
+            self.target.__class__, direction, bypass_extensions=True
+        )
+
+        # remove description because it uses GegevensGroepSerializer docstring
+        # the parent serializer will add a description from help text for this component
+        del schema["description"]
+
+        return schema
