e666f493646608190b0375f5151ef7a7703fb94d
Anna Shamray <anna.shamray@maykinmedia.nl>
2023-12-27 14:11:33 +0100

    :recycle: [#1510] change command to generate OAS for Windows

--- bin/generate_schema_for_component.cmd
+++ bin/generate_schema_for_component.cmd
@@ -16,27 +16,10 @@ if "%1"=="" (
 set SUBPATH=/%1/api
 set SCHEMA_PATH=src/openzaak/components/%1
 
-echo Generating Swagger schema for %1...
-python src\manage.py generate_swagger_component^
-    %SCHEMA_PATH%\swagger2.0.json^
-    --overwrite^
-    --format=json^
-    --mock-request^
-    --url https://example.com/api/v1^
-    --component=%1
+echo "Generating OAS schema for $1..."
+python src/manage.py spectacular_for_component^
+    --file $SCHEMA_PATH/openapi.yaml^
+    --component
 
-echo Converting Swagger to OpenAPI 3.0...
-call .\node_modules\.bin\swagger2openapi %SCHEMA_PATH%\swagger2.0.json -o %SCHEMA_PATH%\openapi.yaml
-REM call npm run convert
-python src\manage.py patch_error_contenttypes %SCHEMA_PATH%\openapi.yaml
-
-echo Generating resources document...
-python src\manage.py generate_swagger_component^
-    %SCHEMA_PATH%\resources.md^
-    --overwrite^
-    --mock-request^
-    --url https://example.com/api/v1^
-    --to-markdown-table^
-    --component=%1
 
 echo Done.
