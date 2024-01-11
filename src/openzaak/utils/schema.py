eaec8de30d1582a3645149ffb27c33b733bf16c9
Anna Shamray <anna.shamray@maykinmedia.nl>
2024-01-11 18:53:24 +0100

    :sparkles: [#1510] support expand in OAS

--- src/openzaak/utils/schema.py
+++ src/openzaak/utils/schema.py
@@ -6,9 +6,19 @@ from collections import OrderedDict
 from typing import Dict, List, Type
 
 from django.conf import settings
+from django.utils.module_loading import import_string
+from django.utils.translation import gettext_lazy as _
 
-from drf_spectacular.openapi import AutoSchema as _AutoSchema
+from drf_spectacular.openapi import (
+    AutoSchema as _AutoSchema,
+    ResolvedComponent,
+    append_meta,
+    build_array_type,
+    build_object_type,
+    is_list_serializer,
+)
 from drf_yasg import openapi
+from furl import furl
 from rest_framework import exceptions, serializers, status
 from vng_api_common.exceptions import PreconditionFailed
 from vng_api_common.geo import GeoMixin
@@ -24,7 +34,9 @@ from vng_api_common.permissions import get_required_scopes
 from vng_api_common.serializers import FoutSerializer, ValidatieFoutSerializer
 from vng_api_common.views import ERROR_CONTENT_TYPE
 
+from .expansion import EXPAND_KEY
 from .middleware import WARNING_HEADER
+from .mixins import ExpandMixin
 from .permissions import AuthRequired
 
 logger = logging.getLogger(__name__)
@@ -56,6 +68,23 @@ COMMON_ERROR_RESPONSES = {status: FoutSerializer for status in COMMON_ERROR_STAT
 VALIDATION_ERROR_RESPONSES = {status.HTTP_400_BAD_REQUEST: ValidatieFoutSerializer}
 FILE_ERROR_RESPONSES = {status.HTTP_413_REQUEST_ENTITY_TOO_LARGE: FoutSerializer}
 PRECONDITION_ERROR_RESPONSES = {status.HTTP_412_PRECONDITION_FAILED: FoutSerializer}
+
+
+def get_component_from_serializer(serializer: serializers.Serializer) -> str:
+    return serializer.Meta.model._meta.app_label
+
+
+def get_external_schema_ref(serializer: serializers.Serializer) -> str:
+    """
+    Constructs the schema references for external resource
+    """
+    component = get_component_from_serializer(serializer)
+    oas_url = settings.EXTERNAL_API_MAPPING[component].oas_url
+    resource_name = serializer.Meta.model._meta.object_name
+
+    f = furl(oas_url)
+    f.fragment.path = f"/components/schemas/{resource_name}"
+    return f.url
 
 
 class OldAutoSchema(_OldAutoSchema):
@@ -237,7 +266,10 @@ class AutoSchema(_AutoSchema):
     def _get_response_for_code(
         self, serializer, status_code, media_types=None, direction="response"
     ):
-        """choose media types and set descriptions"""
+        """
+        choose media types and set descriptions
+        add custom response for expand
+        """
         if not media_types:
             if int(status_code) >= 400:
                 media_types = [ERROR_CONTENT_TYPE]
@@ -245,8 +277,11 @@ class AutoSchema(_AutoSchema):
                 media_types = ["application/json"]
 
         response = super()._get_response_for_code(
-            serializer, status_code, media_types, direction="response"
+            serializer, status_code, media_types, direction
         )
+
+        if 200 <= int(status_code) < 300 and isinstance(self.view, ExpandMixin):
+            response = self.get_expand_response(serializer, response, direction)
 
         # add description based on the status code
         if not response.get("description"):
@@ -283,3 +318,90 @@ class AutoSchema(_AutoSchema):
             return self.view.paginator
 
         return super()._get_paginator()
+
+    def get_expand_response(self, serializer, base_response, direction):
+        """
+        add '_expand' into response schema
+        """
+
+        include_allowed = getattr(self.view, "include_allowed", lambda: False)()
+        base_serializer = (
+            serializer.child if is_list_serializer(serializer) else serializer
+        )
+        inclusion_serializers = getattr(base_serializer, "inclusion_serializers", {})
+
+        if not include_allowed or not inclusion_serializers:
+            return base_response
+
+        response = base_response.copy()
+        # rewrite schema from response
+        expand_properties = {}
+        for name, serializer_class in inclusion_serializers.items():
+            # create schema for top-level inclusions for now
+            if "." in name:
+                continue
+
+            inclusion_field = base_serializer.fields[name]
+            meta = self._get_serializer_field_meta(inclusion_field, direction)
+            inclusion_serializer = import_string(serializer_class)
+
+            if get_component_from_serializer(
+                base_serializer
+            ) == get_component_from_serializer(inclusion_serializer):
+                # same component - local ref
+                inclusion_ref = self.resolve_serializer(
+                    inclusion_serializer, direction
+                ).ref
+            else:
+                # external component - external ref
+                inclusion_ref = {"$ref": get_external_schema_ref(inclusion_serializer)}
+
+            many = True if hasattr(inclusion_field, "child_relation") else False
+            if many:
+                inclusion_schema = append_meta(build_array_type(inclusion_ref), meta)
+            else:
+                inclusion_schema = append_meta(inclusion_ref, meta)
+
+            expand_properties[name] = inclusion_schema
+
+        inclusions_schema = build_object_type(
+            properties=expand_properties,
+            description=_(
+                "Display details of the linked resources requested in the `expand` parameter"
+            ),
+        )
+        base_component = self.resolve_serializer(base_serializer, direction)
+        expand_component_name = f"Expand{base_component.name}"
+        expand_component = ResolvedComponent(
+            name=expand_component_name,
+            type=ResolvedComponent.SCHEMA,
+            object=expand_component_name,
+            schema={
+                "allOf": [
+                    base_component.ref,
+                    build_object_type(properties={EXPAND_KEY: inclusions_schema}),
+                ]
+            },
+        )
+        self.registry.register_on_missing(expand_component)
+        expand_schema = expand_component.ref
+
+        # paginate if needed
+        if self._is_list_view(serializer):
+            expand_schema = build_array_type(expand_schema)
+
+            paginator = self._get_paginator()
+            if paginator:
+                paginated_name = self.get_paginated_name(expand_component_name)
+                paginated_component = ResolvedComponent(
+                    name=paginated_name,
+                    type=ResolvedComponent.SCHEMA,
+                    schema=paginator.get_paginated_response_schema(expand_schema),
+                    object=paginated_name,
+                )
+                self.registry.register_on_missing(paginated_component)
+                expand_schema = paginated_component.ref
+
+        response["content"]["application/json"]["schema"] = expand_schema
+
+        return response
