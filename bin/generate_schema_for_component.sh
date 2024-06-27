cee3fd4302b52908d8f19303f57474138938ba2e
Sonny Bakker <sonny@maykinmedia.nl>
2024-06-27 13:53:42 +0200

    [#17] add `CI_JOB` variable to detect CI job

--- bin/generate_schema_for_component.sh
+++ bin/generate_schema_for_component.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-if [[ -z "$VIRTUAL_ENV" ]]; then
+if [[ -z "$VIRTUAL_ENV" ]] && [[ ! -v CI_JOB ]]; then
     echo "You need to activate your virtual env before running this script"
     exit 1
 fi
