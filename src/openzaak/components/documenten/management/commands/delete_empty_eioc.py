8b1f2f7358ef0b0f4c526a322fa3101650692008
Anna Shamray <anna.shamray@maykinmedia.nl>
2024-09-19 17:54:17 +0200

    :recycle: [#1766] use apps.get_model in documenten 0034 migration

--- src/openzaak/components/documenten/management/commands/delete_empty_eioc.py
+++ src/openzaak/components/documenten/management/commands/delete_empty_eioc.py
@@ -1,15 +1,62 @@
 # SPDX-License-Identifier: EUPL-1.2
 # Copyright (C) 2024 Dimpact
-from typing import List
+from collections.abc import Callable
+from typing import List, Type
 
 from django.conf import settings
 from django.core.management import BaseCommand
-from django.db import transaction
+from django.db import models, transaction
 from django.utils.translation import gettext_lazy as _
 
 from openzaak.components.besluiten.models import BesluitInformatieObject
 from openzaak.components.documenten.models import EnkelvoudigInformatieObjectCanonical
 from openzaak.components.zaken.models import ZaakInformatieObject
+
+
+def delete_eioc_and_relations(
+    eioc_model: Type[models.Model],
+    zio_model: Type[models.Model],
+    bio_model: Type[models.Model],
+    log_func: Callable[
+        [
+            str,
+        ],
+        None,
+    ],
+) -> None:
+    """
+    models are added as parameters to be able to use this function in the migrations with apps.get_model
+    """
+    msg = "Checking {count} records ...".format(count=eioc_model.objects.count())
+    log_func(msg)
+
+    duplicates = eioc_model.objects.filter(enkelvoudiginformatieobject__isnull=True)
+
+    if len(duplicates) == 0:
+        log_func("Found no empty records.")
+        return
+
+    msg = _("Found {count} empty records.").format(count=len(duplicates))
+    log_func(msg)
+
+    related_ziots = zio_model.objects.filter(_informatieobject__in=duplicates)
+    related_biots = bio_model.objects.filter(_informatieobject__in=duplicates)
+
+    msg = (
+        "Found {biot_count} related BesluitInformatieObject "
+        "and ZaakInformatieObject related {ziot_count}."
+    ).format(biot_count=related_biots.count(), ziot_count=related_ziots.count())
+    log_func(msg)
+
+    with transaction.atomic():
+        delete_empties(related_ziots)
+        delete_empties(related_biots)
+        delete_empties(duplicates)
+
+
+def delete_empties(empties: List) -> None:
+    for document_id in empties:
+        document_id.delete()
 
 
 class Command(BaseCommand):
@@ -20,41 +67,9 @@ class Command(BaseCommand):
             self.stdout.write("This command does not run with CMIS enabled.")
             return
 
-        msg = "Checking {count} records ...".format(
-            count=EnkelvoudigInformatieObjectCanonical.objects.count()
+        delete_eioc_and_relations(
+            eioc_model=EnkelvoudigInformatieObjectCanonical,
+            zio_model=ZaakInformatieObject,
+            bio_model=BesluitInformatieObject,
+            log_func=self.stdout.write,
         )
-        self.stdout.write(msg)
-
-        duplicates = EnkelvoudigInformatieObjectCanonical.objects.filter(
-            enkelvoudiginformatieobject__isnull=True
-        )
-
-        if len(duplicates) == 0:
-            self.stdout.write("Found no empty records.")
-            return
-
-        msg = _("Found {count} empty records.").format(count=len(duplicates))
-        self.stdout.write(msg)
-
-        related_ziots = ZaakInformatieObject.objects.filter(
-            _informatieobject__in=duplicates
-        )
-        related_biots = BesluitInformatieObject.objects.filter(
-            _informatieobject__in=duplicates
-        )
-
-        msg = (
-            "Found {biot_count} related BesluitInformatieObject "
-            "and ZaakInformatieObject related {ziot_count}."
-        ).format(biot_count=related_biots.count(), ziot_count=related_ziots.count())
-        self.stdout.write(msg)
-
-        with transaction.atomic():
-            delete_empties(related_ziots)
-            delete_empties(related_biots)
-            delete_empties(duplicates)
-
-
-def delete_empties(empties: List) -> None:
-    for document_id in empties:
-        document_id.delete()
