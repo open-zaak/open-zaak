953b80ec7cb0451b8b939f6ae9a015a05c15f2fd
Anna Shamray <anna.shamray@maykinmedia.nl>
2024-09-20 16:32:11 +0200

    :white_check_mark: [#1766] test documenten migration

--- src/openzaak/components/documenten/tests/management/test_delete_empty_eioc.py
+++ src/openzaak/components/documenten/tests/management/test_delete_empty_eioc.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: EUPL-1.2
 # Copyright (C) 2024 Dimpact
 
+from datetime import date, timedelta
+
 from django.core.management import call_command
 
 from rest_framework import status
@@ -18,7 +20,7 @@ from openzaak.components.zaken.tests.factories import (
     ZaakInformatieObjectFactory,
 )
 from openzaak.components.zaken.tests.utils import ZAAK_WRITE_KWARGS
-from openzaak.tests.utils import JWTAuthMixin
+from openzaak.tests.utils import JWTAuthMixin, TestMigrations
 
 from ...models import EnkelvoudigInformatieObjectCanonical
 from ..factories import EnkelvoudigInformatieObjectCanonicalFactory
@@ -66,3 +68,101 @@ class DeleteEmptyEIOCTests(JWTAuthMixin, APITestCase):
         self.assertEqual(EnkelvoudigInformatieObjectCanonical.objects.count(), 0)
         self.assertEqual(ZaakInformatieObject.objects.count(), 0)
         self.assertEqual(BesluitInformatieObject.objects.count(), 0)
+
+
+class DeleteEmptyEIOCMigrationTests(TestMigrations):
+    """
+    regression test for https://github.com/open-zaak/open-zaak/issues/1766
+    """
+
+    migrate_from = "0033_alter_enkelvoudiginformatieobject_identificatie"
+    migrate_to = "0034_delete_empty_eioc"
+    app = "documenten"
+
+    def setUpBeforeMigration(self, apps):
+        """
+        set up zaken and documenten
+        """
+        Catalogus = apps.get_model("catalogi", "Catalogus")
+        ZaakType = apps.get_model("catalogi", "ZaakType")
+        EnkelvoudigInformatieObjectCanonical = apps.get_model(
+            "documenten", "EnkelvoudigInformatieObjectCanonical"
+        )
+        ObjectInformatieObject = apps.get_model("documenten", "ObjectInformatieObject")
+        # EnkelvoudigInformatieObject = self.old_state.apps.get_model("documenten", "EnkelvoudigInformatieObject")
+        ZaakIdentificatie = apps.get_model("zaken", "ZaakIdentificatie")
+        Zaak = apps.get_model("zaken", "Zaak")
+        ZaakInformatieObject = apps.get_model("zaken", "ZaakInformatieObject")
+
+        # use bulk create to avoid etag calculation trigger
+        # catalogi
+        catalogus = Catalogus(
+            domein="AAAAA",
+            naam="Some Catalogus",
+            rsin="000000000",
+        )
+        Catalogus.objects.bulk_create([catalogus])
+        zaaktype = ZaakType(
+            catalogus=catalogus,
+            zaaktype_omschrijving="zaaktype 1",
+            vertrouwelijkheidaanduiding="openbaar",
+            doel="some goal",
+            aanleiding="test",
+            indicatie_intern_of_extern="extern",
+            handeling_initiator="test",
+            onderwerp="test",
+            handeling_behandelaar="test",
+            doorlooptijd_behandeling=timedelta(days=10),
+            opschorting_en_aanhouding_mogelijk=True,
+            verlenging_mogelijk=True,
+            publicatie_indicatie=True,
+            versiedatum=date(2024, 1, 1),
+            verantwoordelijke="063308836",
+            referentieproces_naam="test",
+            datum_begin_geldigheid=date(2024, 1, 1),
+        )
+        ZaakType.objects.bulk_create([zaaktype])
+
+        # documenten
+        canonical = EnkelvoudigInformatieObjectCanonical.objects.create()
+
+        # zaken
+        zaak_identificatie = ZaakIdentificatie.objects.create(
+            bronorganisatie="517439943",
+            identificatie="ZAAK1",
+        )
+        zaak = Zaak(
+            identificatie_ptr=zaak_identificatie,
+            _zaaktype=zaaktype,
+            verantwoordelijke_organisatie="063308836",
+            startdatum=date(2024, 1, 1),
+            vertrouwelijkheidaanduiding="openbaar",
+        )
+        Zaak.objects.bulk_create([zaak])
+        zaak.refresh_from_db()
+        # relations
+        zio = ZaakInformatieObject(
+            zaak=zaak,
+            _informatieobject=canonical,
+            aard_relatie="hoort_bij",
+        )
+        ZaakInformatieObject.objects.bulk_create([zio])
+        ObjectInformatieObject.objects.create(
+            informatieobject=canonical,
+            object_type="zaak",
+            _zaak=zaak,
+        )
+
+    def test_empty_canonical_removed(self):
+        ZaakInformatieObject = self.apps.get_model("zaken", "ZaakInformatieObject")
+        EnkelvoudigInformatieObjectCanonical = self.apps.get_model(
+            "documenten", "EnkelvoudigInformatieObjectCanonical"
+        )
+        ObjectInformatieObject = self.apps.get_model(
+            "documenten", "ObjectInformatieObject"
+        )
+
+        self.assertEqual(EnkelvoudigInformatieObjectCanonical.objects.count(), 0)
+        self.assertEqual(ZaakInformatieObject.objects.count(), 0)
+        self.assertEqual(ObjectInformatieObject.objects.count(), 0)
+        self.assertEqual(ZaakInformatieObject.objects.count(), 0)
