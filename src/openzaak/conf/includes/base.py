ee11f79ce3a891e710f3f2337e64efcac1abdd90
Anna Shamray <anna.shamray@maykinmedia.nl>
2024-12-31 16:18:19 +0100

    :sparkles: [#1825] add commongground-api-common steps to OZ configuration

--- src/openzaak/conf/includes/base.py
+++ src/openzaak/conf/includes/base.py
@@ -293,6 +293,8 @@ SETUP_CONFIGURATION_STEPS = [
     "django_setup_configuration.config.sites.steps.SitesConfigurationStep",
     "zgw_consumers.contrib.setup_configuration.steps.ServiceConfigurationStep",
     "notifications_api_common.contrib.setup_configuration.steps.NotificationConfigurationStep",
+    "vng_api_common.contrib.setup_configuration.steps.JWTSecretsConfigurationStep",
+    "vng_api_common.contrib.setup_configuration.steps.ApplicatieConfigurationStep",
     # "openzaak.config.setup_configuration.steps.SelectielijstAPIConfigurationStep",
     # "openzaak.config.setup_configuration.steps.DemoUserStep",
 ]
