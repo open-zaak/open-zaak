2ba1d9e43435f37b2a84150c02076b04fa1a9ccd
Anna Shamray <anna.shamray@maykinmedia.nl>
2025-01-31 10:33:28 +0100

    :ok_hand: [#1855] process PR feedback

--- src/openzaak/components/catalogi/tests/test_informatieobjecttype.py
+++ src/openzaak/components/catalogi/tests/test_informatieobjecttype.py
@@ -763,6 +763,22 @@ class InformatieObjectTypeFilterAPITests(APITestCase):
         self.assertEqual(len(data), 1)
         self.assertEqual(data[0]["url"], f"http://openzaak.nl{reverse(iotype)}")
 
+    @override_settings(ALLOWED_HOSTS=["openzaak.nl"])
+    def test_filter_zaaktype_not_exist(self):
+        InformatieObjectTypeFactory.create(omschrijving="some", concept=False)
+        zaaktype_url = reverse(
+            "zaaktype-detail", kwargs={"uuid": "221e7626-a556-4eb5-9714-e7693f82c2dd"}
+        )
+
+        response = self.client.get(
+            self.url,
+            {"zaaktype": f"http://openzaak.nl{zaaktype_url}"},
+            headers={"host": "openzaak.nl"},
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["count"], 0)
+
 
 class InformatieObjectTypePaginationTestCase(APITestCase):
     maxDiff = None
