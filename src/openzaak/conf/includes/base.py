5bd31fa055607603d7e968671923edf13023b194
Floris272 <floris@maykinmedia.nl>
2025-02-10 11:31:45 +0100

    [#1616] update env_config

--- src/openzaak/conf/includes/base.py
+++ src/openzaak/conf/includes/base.py
@@ -356,7 +356,8 @@ ZAAK_IDENTIFICATIE_GENERATOR_OPTIONS = {
 ZAAK_IDENTIFICATIE_GENERATOR = config(
     "ZAAK_IDENTIFICATIE_GENERATOR",
     default="use-start-datum-year",
-    help_text="Select one of the options defined in ZAAK_IDENTIFICATIE_GENERATOR_OPTIONS "
+    help_text="Select one of the options defined in ZAAK_IDENTIFICATIE_GENERATOR_OPTIONS: "
+    f"[{', '.join(ZAAK_IDENTIFICATIE_GENERATOR_OPTIONS.keys())}] "
     "that define how the ZAAK identificatie is generated.",
 )
 
