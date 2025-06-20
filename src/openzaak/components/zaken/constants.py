b71fef92f28ca282b03ca2a6d13619390049ff06
OlhaZahoruiko <olha@maykinmedia.nl>
2025-06-20 18:01:02 +0200

    fixup! :sparkles: [open-zaak#1148] Add SubStatus resource to Zaken API

--- src/openzaak/components/zaken/constants.py
+++ src/openzaak/components/zaken/constants.py
@@ -3,7 +3,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from django_choices import ChoiceItem, DjangoChoices
+from djchoices import ChoiceItem, DjangoChoices
 
 from openzaak.utils.help_text import mark_experimental
 
