3bfb934aba99e5daa74f6ced2161c4d8631adc54
Floris272 <floris@maykinmedia.nl>
2025-06-24 17:01:50 +0200

    :recycle: [#635] refact BronDatumTests by patching get_selectielijstklasse

--- src/openzaak/components/zaken/tests/test_brondatum.py
+++ src/openzaak/components/zaken/tests/test_brondatum.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: EUPL-1.2
 # Copyright (C) 2025 Dimpact
 from datetime import datetime
+from unittest.mock import patch
 
 from django.test import TestCase
 
@@ -8,11 +9,16 @@ from openzaak.components.zaken.brondatum import BrondatumCalculator
 from openzaak.components.zaken.tests.factories import ResultaatFactory, ZaakFactory
 
 
+@patch(
+    "openzaak.components.catalogi.models.resultaattype.ResultaatType.get_selectielijstklasse",
+    return_value={"bewaartermijn": None},
+)
 class BronDatumTests(TestCase):
-    def test_calculate_brondatum_without_archiefactietermijn(self):
+    def test_calculate_brondatum_without_archiefactietermijn(
+        self, mock_get_selectielijstklasse
+    ):
         zaak = ZaakFactory()
-        resultaat = ResultaatFactory(zaak=zaak)
         # when archiefactietermijn is not set it is set by the selectielijstklasse
-        resultaat.resultaattype.archiefactietermijn = None
+        ResultaatFactory(zaak=zaak, resultaattype__archiefactietermijn=None)
         calculator = BrondatumCalculator(zaak, datetime.now())
         self.assertIsNone(calculator.calculate())
