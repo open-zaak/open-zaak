6d5bc5e803423d2127b00b5cd38b5fdfbc8bd12e
Floris272 <floris@maykinmedia.nl>
2025-07-03 17:43:11 +0200

    :white_check_mark: [#2064] add informatieobject to expected response

--- src/openzaak/components/zaken/api/permissions.py
+++ src/openzaak/components/zaken/api/permissions.py
@@ -25,7 +25,9 @@ class ZaakNestedAuthRequired(ZaakAuthRequired):
         component = self.get_component(view)
 
         main_object = view._get_zaak()
-        main_object_data = self.format_data(main_object, request, self.get_main_resource(self.main_resource))
+        main_object_data = self.format_data(
+            main_object, request, self.get_main_resource(self.main_resource)
+        )
 
         fields = self.get_fields(main_object_data, self.permission_fields)
         return request.jwt_auth.has_auth(scopes_required, component, **fields)
