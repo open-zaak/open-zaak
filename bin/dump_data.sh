cab2980a9ec945d5d217bb661fd448e786ccd3b9
Floris272 <floris@maykinmedia.nl>
2025-07-18 11:11:00 +0200

    :sparkles: [maykinmedia/open-api-framework#157] add flags to data_dump script

--- bin/dump_data.sh
+++ bin/dump_data.sh
@@ -5,14 +5,15 @@
 # dump_data.sh for all apps or dump_data.sh zaken documenten ... to specify specific apps
 # Note that postgres 17 requires postgres-client-17
 #
-# This script dumps the whole db schema and adds appends the datadump with specified tables.
-# The schema dump cannot use -t to filter tables because this excludes extensions like postgis in the dump.
+# By default a schema and data dump are created separately. This can be changed with the flags --data-only, --schema-only
+# or --combined which appends the data dump to the schema dump.
+# The schema dump could not use -t to filter tables because this excludes extensions like postgis in the dump.
 # pg_dump also does not add related tables automatically, so `dump_data.sh zaken` does not add related zaaktype data to the dump.
 
 
 set -e
 
-DEFAULT_APPS=(besluiten catalogi documenten zaken) # zgw_consumers simple_certmanager
+DEFAULT_APPS=(besluiten catalogi documenten zaken)
 
 export PGHOST=${DB_HOST:-db}
 export PGPORT=${DB_PORT:-5432}
@@ -27,10 +28,26 @@ ${SCRIPTPATH}/wait_for_db.sh
 
 DUMP_FILE=${DUMP_FILE:-"dump_$(date +'%Y-%m-%d_%H-%M-%S').sql"}
 
+SCHEMA=true
+DATA=true
+COMBINED=false
+
+for arg in "$@"; do
+  case "$arg" in
+    --schema-only) DATA=false ;;
+    --data-only)   SCHEMA=false ;;
+    --combined)    COMBINED=true ;;
+    --*)
+      echo "Unknown flag: $arg"
+      exit 1
+      ;;
+    *)
+      APPS+=("$arg") ;;
+  esac
+done
+
 # export given apps or export DEFAULT_APPS
-if [ "$#" -gt 0 ]; then
-  APPS=("$@")
-else
+if [ "${#APPS[@]}" -eq 0 ]; then
   APPS=("${DEFAULT_APPS[@]}")
 fi
 
@@ -42,10 +59,33 @@ for app in "${APPS[@]}"; do
     INCLUDES+=("-t" "${app}_*")
 done
 
-# dump full schema
-pg_dump --schema-only -f "$DUMP_FILE"
+dump_schema() {
+  echo "Dumping schema to $1..."
+  pg_dump --schema-only -f "$1"
+}
 
-# dump data of tables added to INCLUDES
-pg_dump "${INCLUDES[@]}" --disable-triggers --data-only | sed '/^SET\|^SELECT pg_catalog.set_config/d' >> "$DUMP_FILE"
+dump_data() {
+  echo "Dumping data to $1..."
+  pg_dump "${INCLUDES[@]}" --disable-triggers --data-only > "$1"
+}
 
->&2 echo "database was exported to $DUMP_FILE"
+append_data() {
+  echo "Appending data to $1..."
+  pg_dump "${INCLUDES[@]}" --disable-triggers --data-only \
+    | sed '/^SET\|^SELECT pg_catalog.set_config/d' >> "$1"
+}
+
+
+if $COMBINED; then
+  dump_schema "$DUMP_FILE"
+  append_data "$DUMP_FILE"
+  exit 0
+fi
+
+if $SCHEMA; then
+  dump_schema "schema__$DUMP_FILE"
+fi
+
+if $DATA; then
+  dump_data "data__$DUMP_FILE"
+fi
