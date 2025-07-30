394e32ba0cf9c92010b43d1fd715babd0f9edc34
Floris272 <floris@maykinmedia.nl>
2025-07-30 12:19:38 +0200

    :sparkles: [#2065] add zaak_opschorten endpoint

--- src/openzaak/components/zaken/api/urls.py
+++ src/openzaak/components/zaken/api/urls.py
@@ -20,6 +20,7 @@ from .viewsets import (
     ZaakEigenschapViewSet,
     ZaakInformatieObjectViewSet,
     ZaakObjectViewSet,
+    ZaakOpschortenViewset,
     ZaakRegistrerenViewset,
     ZaakVerzoekViewSet,
     ZaakViewSet,
@@ -52,6 +53,8 @@ router.register(
     basename="zaakidentificatie_alias",
 )
 router.register("zaak_registreren", ZaakRegistrerenViewset, basename="registreerzaak")
+zaakopschorten_view = ZaakOpschortenViewset.as_view({"post": "post"})
+
 
 urlpatterns = [
     re_path(
@@ -77,6 +80,11 @@ urlpatterns = [
                 # actual API
                 path("", include(router.urls)),
                 path("", router.APIRootView.as_view(), name="api-root-zaken"),
+                path(
+                    "zaak_opschorten/<uuid:uuid>",
+                    zaakopschorten_view,
+                    name="schortzaakop",
+                ),
                 path("", include("vng_api_common.notifications.api.urls")),
             ]
         ),
