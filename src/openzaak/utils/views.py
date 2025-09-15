bef4be8fad0fc4983ed8ba5eecf41a5c1778f883
Daniel Mursa <daniel@maykinmedia.nl>
2025-09-15 12:13:37 +0200

    :fire: Remove obsolete files and commands
    - 500.html
    - checks.py -> check_modelform_exclude, check_missing_init_files
    - views.py -> server_error

--- src/openzaak/utils/views.py
+++ src/openzaak/utils/views.py
@@ -1,9 +1,5 @@
 # SPDX-License-Identifier: EUPL-1.2
 # Copyright (C) 2019 - 2020 Dimpact
-from django import http
-from django.template import TemplateDoesNotExist, loader
-from django.views.decorators.csrf import requires_csrf_token
-from django.views.defaults import ERROR_500_TEMPLATE_NAME
 
 from rest_framework import exceptions, viewsets
 from rest_framework.views import APIView
@@ -13,27 +9,6 @@ from vng_api_common.views import (
     _test_nrc_config,
     _test_sites_config,
 )
-
-
-@requires_csrf_token
-def server_error(request, template_name=ERROR_500_TEMPLATE_NAME):
-    """
-    500 error handler.
-
-    Templates: :template:`500.html`
-    Context: None
-    """
-    try:
-        template = loader.get_template(template_name)
-    except TemplateDoesNotExist:
-        if template_name != ERROR_500_TEMPLATE_NAME:
-            # Reraise if it's a missing custom template.
-            raise
-        return http.HttpResponseServerError(
-            "<h1>Server Error (500)</h1>", content_type="text/html"
-        )
-    context = {"request": request}
-    return http.HttpResponseServerError(template.render(context))
 
 
 class ViewConfigView(_ViewConfigView):
