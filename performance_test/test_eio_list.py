adc99251ef376149cfb1e63b7d3fa7a474bc1b6e
Floris272 <floris@maykinmedia.nl>
2025-10-14 13:00:31 +0200

    :green_heart: [#1653] fix eio performance test urls

--- performance_test/test_eio_list.py
+++ performance_test/test_eio_list.py
@@ -5,7 +5,7 @@ import pytest
 import requests
 from furl import furl
 
-BASE_URL = furl("http://localhost:8000/zaken/api/v1/")
+BASE_URL = furl("http://localhost:8000/documenten/api/v1/")
 
 
 def generate_token(client_id: str, secret: str) -> str:
@@ -44,7 +44,9 @@ def test_eio_list(benchmark, benchmark_assertions):
     params = {"pageSize": 100, "page": 34}
 
     def make_request():
-        return requests.get((BASE_URL / "zaken").set(params), headers=HEADERS)
+        return requests.get(
+            (BASE_URL / "enkelvoudiginformatieobjecten").set(params), headers=HEADERS
+        )
 
     result = benchmark(make_request)
 
@@ -61,7 +63,9 @@ def test_eio_list_with_ordering(benchmark, benchmark_assertions):
     params = {"pageSize": 100, "page": 34, "ordering": "-auteur"}
 
     def make_request():
-        return requests.get((BASE_URL / "zaken").set(params), headers=HEADERS)
+        return requests.get(
+            (BASE_URL / "enkelvoudiginformatieobjecten").set(params), headers=HEADERS
+        )
 
     result = benchmark(make_request)
 
