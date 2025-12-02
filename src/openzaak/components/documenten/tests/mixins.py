ffb2543008b1378bc35fcd8de30f65a1dda897cd
Steven Bal <steven@maykinmedia.nl>
2025-12-02 13:57:04 +0100

    :white_check_mark: [#2217] Add tests for Azure blob storage backend for Documenten API

---
+++ src/openzaak/components/documenten/tests/mixins.py
@@ -0,0 +1,19 @@
+from django.utils.functional import LazyObject
+
+from ..storage import documenten_storage
+
+
+class AzureBlobStorageMixin:
+    """
+    Mixin to make sure the underlying Documenten storage backend can be overridden
+    with `@override_settings`
+    """
+
+    def setUp(self):
+        super().setUp()
+
+        assert isinstance(documenten_storage, LazyObject)
+
+        # TODO find cleaner solution
+        documenten_storage._wrapped = None  # force reload
+        documenten_storage._setup()
