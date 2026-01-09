f9316efd924015dd554f40931f1864dd9d5cf2be
floris272 <floris@maykinmedia.nl>
2026-01-09 16:28:55 +0100

    :construction_worker: [#2268] add playwright to ci

--- src/openzaak/tests/js/test_objecthistory.py
+++ src/openzaak/tests/js/test_objecthistory.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: EUPL-1.2
+# Copyright (C) 2025 Dimpact
 from django.test import override_settings, tag
 
 from playwright.sync_api import expect
--- src/openzaak/tests/utils/playwright.py
+++ src/openzaak/tests/utils/playwright.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: EUPL-1.2
+# Copyright (C) 2025 Dimpact
 import os
 import tempfile
 from collections.abc import Callable
