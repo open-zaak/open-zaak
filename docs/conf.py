15fa863b055bfca63f04f269d459df85a0005444
Steven Bal <steven@maykinmedia.nl>
2026-03-17 10:46:42 +0100

    :lipstick: [maykinmedia/open-api-framework#213] Consistent styling for docs
    to be in line with other Maykin products/default project

--- docs/conf.py
+++ docs/conf.py
@@ -100,6 +100,9 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = [
+    "theme_overrides.css",  # override wide tables with word wrap
+]
 
 todo_include_todos = True
 
