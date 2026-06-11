19d59a923cf9e59d4f424cde42f4d22967b1d9b3
Steven Bal <steven@maykinmedia.nl>
2026-06-11 14:09:35 +0200

    :bookmark: [#2399] Release version 1.29.0

--- src/openzaak/__init__.py
+++ src/openzaak/__init__.py
@@ -3,6 +3,6 @@
 from .celery import app as celery_app
 
 __all__ = ("celery_app",)
-__version__ = "1.28.1"
+__version__ = "1.29.0"
 __author__ = "Maykin"
 __homepage__ = "https://github.com/open-zaak/open-zaak"
