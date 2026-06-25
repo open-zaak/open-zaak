f6e690bcc929b4633b124178643cf67cf79f1663
floris272 <floris@maykinmedia.nl>
2026-06-25 13:46:53 +0200

    :sparkles: [#2416] add besluiten viewsets to zaken router

--- src/openzaak/components/zaken/api/urls.py
+++ src/openzaak/components/zaken/api/urls.py
@@ -5,6 +5,12 @@ from django.urls import include, path, re_path
 from drf_spectacular.views import SpectacularRedocView
 from vng_api_common import routers
 
+from openzaak.components.besluiten.api.viewsets import (
+    BesluitAuditTrailViewSet,
+    BesluitInformatieObjectViewSet,
+    BesluitVerwerkenViewSet,
+    BesluitViewSet,
+)
 from openzaak.utils.oas_extensions.views import (
     DeprecationRedirectView,
     SchemaDeprecationRedirectView,
@@ -58,6 +64,14 @@ router.register("zaakcontactmomenten", ZaakContactMomentViewSet)
 router.register("zaakverzoeken", ZaakVerzoekViewSet)
 router.register("zaaknummer_reserveren", ReserveerZaakNummerViewSet)
 router.register("zaaknotities", ZaakNotitieViewSet)
+router.register(
+    "besluiten",
+    BesluitViewSet,
+    [routers.Nested("audittrail", BesluitAuditTrailViewSet)],
+)
+router.register("besluitinformatieobjecten", BesluitInformatieObjectViewSet)
+router.register("besluit_verwerken", BesluitVerwerkenViewSet, basename="verwerkbesluit")
+
 
 # XXX: alias for this endpoint, will be removed in 2.0
 router.register(
