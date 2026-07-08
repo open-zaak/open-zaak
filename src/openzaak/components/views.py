edbd3b6d2e6568e37d32442fe47389e15ca5192f
floris272 <floris@maykinmedia.nl>
2026-07-08 13:51:16 +0200

    :recycle: [#2416] added deprecation warnings & minor fixes

--- src/openzaak/components/views.py
+++ src/openzaak/components/views.py
@@ -8,8 +8,10 @@ from maykin_common.api_reference.views import (
 class ComponentIndexView(BaseComponentIndexView):
     template_name = "index.html"
     description = ""
+    deprecated = False
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["description"] = self.description
+        context["deprecated"] = self.deprecated
         return context
