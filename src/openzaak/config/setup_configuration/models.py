0edbd95d005dd41806874790225e68be450afd82
floris272 <floris@maykinmedia.nl>
2026-07-31 18:08:43 +0200

    :sparkles: [#2460] add ApplicatieConfigurationStep from commonground-api-common that uses new autorisatie models

--- src/openzaak/config/setup_configuration/models.py
+++ src/openzaak/config/setup_configuration/models.py
@@ -4,6 +4,7 @@ from django_setup_configuration.fields import DjangoModelRef
 from django_setup_configuration.models import ConfigurationModel
 from pydantic import PositiveInt
 
+from openzaak.components.autorisaties.models import Applicatie
 from openzaak.selectielijst.models import ReferentieLijstConfig
 
 
@@ -25,3 +26,27 @@ class SelectielijstAPIConfig(ConfigurationModel):
                 "default_year",
             ],
         }
+
+
+class SingleApplicatieConfigurationModel(ConfigurationModel):
+    """
+    Copied over from commonground-api-common because of new applicatie model
+    """
+
+    uuid: str
+    client_ids: list[str]
+    label: str
+    heeft_alle_autorisaties: bool
+
+    class Meta:
+        django_model_refs = {
+            Applicatie: ["uuid", "client_ids", "label", "heeft_alle_autorisaties"]
+        }
+        extra_kwargs = {
+            "client_ids": {"examples": [["open-notificaties-prod"]]},
+            "label": {"examples": ["Open Notificaties (productie)"]},
+        }
+
+
+class ApplicatieConfigurationModel(ConfigurationModel):
+    items: list[SingleApplicatieConfigurationModel]
