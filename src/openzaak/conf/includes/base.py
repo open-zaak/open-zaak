0efdb77e20edef98c2d17207d7bfd09068527f8b
floris272 <floris@maykinmedia.nl>
2026-07-31 18:08:43 +0200

    :sparkles: [#2460] add ApplicatieConfigurationStep from commonground-api-common that uses new autorisatie models

--- src/openzaak/conf/includes/base.py
+++ src/openzaak/conf/includes/base.py
@@ -336,7 +336,7 @@ SETUP_CONFIGURATION_STEPS = [
     "zgw_consumers.contrib.setup_configuration.steps.ServiceConfigurationStep",
     "openzaak.config.setup_configuration.steps.SelectielijstAPIConfigurationStep",
     "vng_api_common.contrib.setup_configuration.steps.JWTSecretsConfigurationStep",
-    "vng_api_common.contrib.setup_configuration.steps.ApplicatieConfigurationStep",
+    "openzaak.config.setup_configuration.steps.ApplicatieConfigurationStep",
     "notifications_api_common.contrib.setup_configuration.steps.NotificationConfigurationStep",
 ]
 
