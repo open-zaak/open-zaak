fe122ccdbcab7c717293c2f86899acbb257fb138
Daniel Mursa <daniel@maykinmedia.nl>
2026-08-03 11:44:10 +0200

    :hammer: Add celery_liveness_probe.sh bash script for chart probes

---
+++ bin/celery_worker_liveness_probe.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+QUEUE=${CELERY_WORKER_QUEUE:=celery}
+WORKER_LIVENESS_FILE=${CELERY_LIVENESS_FILE:-/app/tmp/celery_worker_event_loop.live}
+WORKER_MAX_AGE=${CELERY_MAX_AGE:-70}
+
+if [ -z "$CELERY_WORKER_NAME" ]; then
+  WORKER_NAME="${QUEUE}@${HOSTNAME}"
+else
+  if [[ "$CELERY_WORKER_NAME" != *"@"* ]]; then
+    WORKER_NAME="celery@${CELERY_WORKER_NAME}"
+  else
+    WORKER_NAME="${CELERY_WORKER_NAME}"
+  fi
+fi
+
+echo ${CELERY_BROKER_URL}
+echo ${WORKER_NAME}
+
+maykin-common worker-health-check \
+  --broker=${CELERY_BROKER_URL} \
+  --worker-name=${WORKER_NAME} \
+  --liveness-file=${WORKER_LIVENESS_FILE} \
+  --max-age=${WORKER_MAX_AGE}
