1973538ea9503e291a712000956f9131ad0fdc49
Steven Bal <steven@maykinmedia.nl>
2026-08-13 09:26:49 +0200

    :bulb: Add missing SPDX headers to several files

--- src/openzaak/utils/typing.py
+++ src/openzaak/utils/typing.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: EUPL-1.2
+# Copyright (C) 2026 Open Zaak maintainers
+
 from collections.abc import Mapping, Sequence
 
 type JSONPrimitive = str | int | float | bool | None
