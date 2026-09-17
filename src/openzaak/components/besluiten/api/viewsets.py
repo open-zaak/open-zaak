17e58e13e45d494a0994b139420bc28f4c9a75a4
floris272 <floris@maykinmedia.nl>
2026-09-17 14:56:18 +0200

    :memo: [#2474] add deprecation notes to main & index pages

--- src/openzaak/components/besluiten/api/viewsets.py
+++ src/openzaak/components/besluiten/api/viewsets.py
@@ -523,9 +523,7 @@ class BesluitVerwerkenViewSet(
         process_cloudevent(
             type=BESLUIT_VERWERKT,
             subject=str(data["besluit"].uuid),
-            dataref=data["besluit"].get_absolute_api_url(
-                namespace=self.request.resolver_match.namespace
-            ),
+            dataref=data["besluit"].get_absolute_api_url(namespace="zaken"),
             data={
                 "verantwoordelijkeOrganisatie": data[
                     "besluit"
--- src/openzaak/components/catalogi/tests/__init__.py
+++ src/openzaak/components/catalogi/tests/__init__.py
@@ -1,4 +1,4 @@
 # SPDX-License-Identifier: EUPL-1.2
 # Copyright (C) 2019 - 2020 Dimpact
 
-# Catalogi API tests have been duplicated to src/openzaak/components/documenten/tests/informatieobjecttypen & src/openzaak/components/zaken/tests/catalogi because the besluiten API has been deprecated and the resources have been moved
+# Catalogi API tests have been duplicated to src/openzaak/components/documenten/tests/informatieobjecttypen & src/openzaak/components/zaken/tests/catalogi because the catalogi API has been deprecated and the resources have been moved
